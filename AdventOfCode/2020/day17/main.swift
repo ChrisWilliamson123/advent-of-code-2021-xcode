0ce6b818c328d66019d8a25c958d4f066c6b6e96
Chris Williamson <chris.williamson@bbc.co.uk>
2021-12-16 19:39:48 +0000

    Refactored day 17 to use a new struct, MultiDimensionalCoord

--- AdventOfCode/2020/day17/main.swift
+++ AdventOfCode/2020/day17/main.swift
@@ -1,139 +1,125 @@
 import Foundation
 
-func main() throws {
-    let input: [String] = try readInput(fromTestFile: false)
+func main(dimensions: Int, cycles: Int, part: Int, usingTestInput: Bool = false) throws {
+    let input: [String] = try readInput(fromTestFile: usingTestInput)
 
-    var activeCubes: Set<Coord3D> = []
+    var activeCubes: Set<MultiDimensionalCoord> = []
 
     for y in 0..<input.count {
         for x in 0..<input[y].count {
-            let coord = Coord3D(x, y, 0, 0)
+            var coordComponents: [Int] = Array(repeating: 0, count: dimensions)
+            coordComponents[0] = x
+            coordComponents[1] = y
+            let coord = MultiDimensionalCoord(coordComponents)
             if input[y][x] == "#" { activeCubes.insert(coord) }
         }
     }
 
-    let cycles = 6
-
-    var adjacentsMap: [Coord3D: Set<Coord3D>] = [:]
+    var adjacentsMap: [MultiDimensionalCoord: Set<MultiDimensionalCoord>] = [:]
 
     for _ in 0..<cycles {
         var state = activeCubes
-        let boundsToConsider = getBoundsToConsider(activeCubes: activeCubes)
-        for x in boundsToConsider.x {
-            for y in boundsToConsider.y {
-                for z in boundsToConsider.z {
-                    for w in boundsToConsider.w {
-                        let coord = Coord3D(x, y, z, w)
-                        let coordIsActive = activeCubes.contains(coord)
+        let allCoordsToConsider: Set<MultiDimensionalCoord> = getAllCoordsToConsider(dimensions: dimensions, activeCubes: activeCubes)
 
-                        let adjacents = adjacentsMap[coord] ?? coord.adjacents
-                        adjacentsMap[coord] = adjacents
-                        
-                        let numberOfActiveAdjacents = adjacents.reduce(0, { activeCubes.contains($1) ? $0 + 1 : $0 })
+        for coord in allCoordsToConsider {
+            let coordIsActive = activeCubes.contains(coord)
 
-                        if coordIsActive {
-                            if !(numberOfActiveAdjacents == 2 || numberOfActiveAdjacents == 3) {
-                                state.remove(coord)
-                            }
-                        } else {
-                            if numberOfActiveAdjacents == 3 {
-                                state.insert(coord)
-                            }
-                        }
-                    }
+            let adjacents = adjacentsMap[coord] ?? coord.adjacents
+            adjacentsMap[coord] = adjacents
+
+            let numberOfActiveAdjacents = adjacents.reduce(0, { activeCubes.contains($1) ? $0 + 1 : $0 })
+
+            if coordIsActive {
+                if !(numberOfActiveAdjacents == 2 || numberOfActiveAdjacents == 3) {
+                    state.remove(coord)
+                }
+            } else {
+                if numberOfActiveAdjacents == 3 {
+                    state.insert(coord)
                 }
             }
         }
 
         activeCubes = state
     }
+    print("Part \(part):", activeCubes.count)
 }
 
-try main()
+struct MultiDimensionalCoord: Hashable {
+    let components: [Int]
 
-struct Coord3D: Hashable {
-    let x: Int
-    let y: Int
-    let z: Int
-    let w: Int
+    var adjacents: Set<MultiDimensionalCoord> {
+        let adjacents = getAdjacents()
+        var asSet = Set(adjacents.map({ MultiDimensionalCoord($0) }))
+        asSet.remove(self)
+        return asSet
+    }
 
-    var adjacents: Set<Coord3D> {
-        var adjacents: Set<Coord3D> = []
-        for x in x-1...x+1 {
-            for y in y-1...y+1 {
-                for z in z-1...z+1 {
-                    for w in w-1...w+1 {
-                        if x == self.x && y == self.y && z == self.z && w == self.w { continue }
-                        adjacents.insert(.init(x, y, z, w))
-                    }
-                }
+    private func getAdjacents(forComponents: [Int]? = nil) -> [[Int]] {
+        if forComponents == nil {
+            return getAdjacents(forComponents: self.components)
+        }
+
+        if forComponents!.count == 1 {
+            let toChange = forComponents![0]
+            return [[toChange-1], [toChange], [toChange+1]]
+        }
+
+        var results: [[Int]] = []
+        var mutable = forComponents!
+        let last = mutable.popLast()!
+        let prev = getAdjacents(forComponents: mutable)
+        for i in last-1...last+1 {
+            for p in prev {
+                results.append(p + [i])
             }
         }
 
-        return adjacents
+        return results
     }
 
-    init(_ x: Int, _ y: Int, _ z: Int, _ w: Int) {
-        self.x = x
-        self.y = y
-        self.z = z
-        self.w = w
+    init(_ components: [Int]) {
+        self.components = components
     }
 }
 
-//struct Coord4D: Hashable {
-//    let x: Int
-//    let y: Int
-//    let z: Int
-//
-//    var adjacents: Set<Coord3D> {
-//        var adjacents: Set<Coord3D> = []
-//        for x in x-1...x+1 {
-//            for y in y-1...y+1 {
-//                for z in z-1...z+1 {
-//                    if x == self.x && y == self.y && z == self.z { continue }
-//                    adjacents.insert(.init(x, y, z))
-//                }
-//            }
-//        }
-//
-//        return adjacents
-//    }
-//
-//    init(_ x: Int, _ y: Int, _ z: Int) {
-//        self.x = x
-//        self.y = y
-//        self.z = z
-//    }
-//}
+private func getAllCoordsToConsider(dimensions: Int, activeCubes: Set<MultiDimensionalCoord>) -> Set<MultiDimensionalCoord> {
+    let bounds = (0..<dimensions).map({ getBoundsToConsider(for: $0, activeCubes: activeCubes) })
 
-private func getBoundsToConsider(activeCubes: Set<Coord3D>) -> (x: ClosedRange<Int>, y: ClosedRange<Int>, z: ClosedRange<Int>, w: ClosedRange<Int>) {
-    var xMin = Int.max
-    var xMax = Int.min
+    func work(forComponents: [ClosedRange<Int>]) -> [[Int]] {
+        if forComponents.count == 1 {
+            return forComponents[0].map({ [$0] })
+        }
 
-    var yMin = Int.max
-    var yMax = Int.min
+        var results: [[Int]] = []
+        var mutable = forComponents
+        let last = mutable.popLast()!
+        let prev = work(forComponents: mutable)
+        for i in last {
+            for p in prev {
+                results.append(p + [i])
+            }
+        }
 
-    var zMin = Int.max
-    var zMax = Int.min
+        return results
 
-    var wMin = Int.max
-    var wMax = Int.min
+    }
+
+    return Set(work(forComponents: bounds).map({ MultiDimensionalCoord($0) }))
+}
+
+private func getBoundsToConsider(for componentIndex: Int, activeCubes: Set<MultiDimensionalCoord>) -> ClosedRange<Int> {
+    var minC = Int.max
+    var maxC = Int.min
 
     for c in activeCubes {
-        xMin = min(xMin, c.x)
-        xMax = max(xMax, c.x)
-
-        yMin = min(yMin, c.y)
-        yMax = max(yMax, c.y)
-
-        zMin = min(zMin, c.z)
-        zMax = max(zMax, c.z)
-
-        wMin = min(wMin, c.w)
-        wMax = max(wMax, c.w)
+        minC = min(minC, c.components[componentIndex])
+        maxC = max(maxC, c.components[componentIndex])
     }
 
-    return (xMin-1...xMax+1, yMin-1...yMax+1, zMin-1...zMax+1, wMin-1...wMax+1)
+    return (minC-1...maxC+1)
 }
+
+try main(dimensions: 3, cycles: 6, part: 1)
+try main(dimensions: 4, cycles: 6, part: 2)
 
