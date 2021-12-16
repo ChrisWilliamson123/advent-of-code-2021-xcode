ec059dfb7343c7bac543d242cc677d2f5ed02852
Chris Williamson <chris.williamson@bbc.co.uk>
2021-12-16 16:36:43 +0000

    Completed 2020 day 15

--- AdventOfCode/2020/day15/main.swift
+++ AdventOfCode/2020/day15/main.swift
@@ -3,7 +3,44 @@ import Foundation
 func main() throws {
     let input: [String] = try readInput(fromTestFile: false)
 
-    print(input)
+    let numbers: [Int] = input[0].split(separator: ",").map({Int($0)!})
+
+    print(numbers)
+
+    let turns = 30000000
+
+    var numbersSpoken: [Int: (prevPrev: Int?, prev: Int)] = [:]
+    var lastNumberSpoken: Int = 0
+
+    for i in 0..<numbers.count {
+        numbersSpoken[numbers[i]] = (nil, i)
+        lastNumberSpoken = numbers[i]
+    }
+
+    for i in numbers.count..<turns {
+        let lastSpokenNumber = lastNumberSpoken
+        let numberInformation = numbersSpoken[lastSpokenNumber]!
+
+        if numberInformation.prevPrev == nil {
+            if let zeroInfo = numbersSpoken[0] {
+                numbersSpoken[0] = (zeroInfo.prev, i)
+            } else {
+                numbersSpoken[0] = (nil, i)
+            }
+            lastNumberSpoken = 0
+        } else {
+            let turnsApart = numberInformation.prev - numberInformation.prevPrev!
+            if let turnsApartInfo = numbersSpoken[turnsApart] {
+                numbersSpoken[turnsApart] = (turnsApartInfo.prev, i)
+            } else {
+                numbersSpoken[turnsApart] = (nil, i)
+            }
+            lastNumberSpoken = turnsApart
+        }
+
+    }
+
+    print(lastNumberSpoken)
 }
 
 try main()
