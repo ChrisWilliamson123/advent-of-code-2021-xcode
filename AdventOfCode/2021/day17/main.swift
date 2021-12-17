4191b4aaff6282f16fe1c27855fc4e5270c9e831
Chris Williamson <chris.williamson@bbc.co.uk>
2021-12-17 10:29:36 +0000

    Refactored 2021 day 17.

--- AdventOfCode/2021/day17/main.swift
+++ AdventOfCode/2021/day17/main.swift
@@ -7,38 +7,13 @@ func main() throws {
     let xTargetRange = Int(matches[0])!...Int(matches[1])!
     let yTargetRange = Int(matches[2])!...Int(matches[3])!
 
-    func getShotYRange(_ velocity: Int) -> Set<Int> {
-        let ticks = 1000
-        var velocity = velocity
-        var latest = velocity
-        var positions: Set<Int> = [latest]
-        for _ in 0..<ticks {
-            velocity -= 1
-            latest += velocity
-            positions.insert(latest)
-        }
-        return positions
-    }
-
-    let yPositionsHit = getShotYRange(abs(yTargetRange.lowerBound) - 1)
-    print("Part 1:", yPositionsHit.max()!)
-
-    func getXNeeded(_ bound: Int) -> Int {
-        let xDiff = bound - 0
-        var tracker = 0
-        var step = 0
-        var steps = -1
-        while tracker < xDiff {
-            tracker += step
-            step += 1
-            steps += 1
-        }
-        return steps
-    }
-
-    let xRange = getXNeeded(xTargetRange.lowerBound)...xTargetRange.upperBound
     let yRange = yTargetRange.lowerBound...(abs(yTargetRange.lowerBound) - 1)
+
+    // LHS of this range is solving quadratic to get smallest velocity needed to reach target
+    let xRange = Int(round((sqrt(Double(2 * xTargetRange.lowerBound)) - 1) / 2))...xTargetRange.upperBound
+
     var valids: Set<Coordinate> = []
+    var maxY = Int.min
     for x in xRange {
         for y in yRange {
             var position = Coordinate(0, 0)
@@ -48,19 +23,13 @@ func main() throws {
             while (!xTargetRange.contains(position.x) || !yTargetRange.contains(position.y)) && ticks < 500 {
                 position = Coordinate(position.x + velocity.x, position.y + velocity.y)
 
-                let xVel: Int
-                if velocity.x > 0 {
-                    xVel = velocity.x - 1
-                } else if velocity.x < 0 {
-                    xVel = velocity.x + 1
-                } else {
-                    xVel = 0
-                }
+                var xVel = velocity.x
+                if velocity.x > 0 { xVel -= 1 }
+                else if velocity.x < 0 { xVel += 1 }
 
-                let yVel = velocity.y - 1
-
-                velocity = Coordinate(xVel, yVel)
+                velocity = Coordinate(xVel, velocity.y - 1)
                 ticks += 1
+                maxY = max(maxY, position.y)
             }
             if ticks > 499 {
                 continue
@@ -68,9 +37,8 @@ func main() throws {
             valids.insert(initialV)
         }
     }
-
+    print("Part 1:", maxY)
     print("Part 2:", valids.count)
-
 }
 
 try main()
