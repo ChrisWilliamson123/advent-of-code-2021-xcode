6eaa31687c08f660b36f14246c042c458c4975d7
Chris Williamson <chris.williamson@bbc.co.uk>
2021-12-20 15:57:02 +0000

    Managed to match the second example scanner to the first after using rotation 11 (index 10)

--- AdventOfCode/2021/day19/main.swift
+++ AdventOfCode/2021/day19/main.swift
@@ -1,260 +1,308 @@
 import simd
-import Foundation
-
-extension simd_float4 {
-    var xyz: simd_float3 {
-        return simd_float3(x, y, z)
-    }
-
-//    init(_ vec3: float3, _ w: Float) {
-//        self = float4(vec3.x, vec3.y, vec3.z, w)
-//    }
-}
-
-extension simd_float3 {
-    var in4d: simd_float4 {
-        simd_float4(self, 1)
-    }
-}
-
 
 func main() throws {
-    let rotationMatrices: [float3x3] = [
-        float3x3(rows:[simd_float3(1,    0,    0),
-        simd_float3(0,    1,    0),
-        simd_float3(0,    0,    1)]),
+//    testTranslationsAndRotations()
+//    test2DMatchingScanners()
+    test3DMatchingScanners()
+    
+}
 
-        float3x3(rows:[simd_float3(1,    0,    0),
-        simd_float3(0,    0,    -1),
-        simd_float3(0,    1,    0)]),
+private func getAllRotatedCoords(using rotation: float4x4, on coords: [simd_float4]) -> [simd_float4] {
+    coords.map({ rotation * $0 })
+}
 
-        float3x3(rows:[simd_float3(1,    0,    0),
-        simd_float3(0,    -1,    0),
-        simd_float3(0,    0,    -1)]),
+private func perform2DTranslation(origin: simd_float3, tx: Float, ty: Float) -> simd_float3 {
+    var transformationMatrix = matrix_identity_float3x3
+    transformationMatrix[2][0] = tx
+    transformationMatrix[2][1] = ty
+    return transformationMatrix * origin
+}
 
-        float3x3(rows:[simd_float3(1,    0,    0),
-        simd_float3(0,    0,    1),
-        simd_float3(0,    -1,    0)]),
+private func perform3DTranslation(origin: simd_float4, tx: Float, ty: Float, tz: Float) -> simd_float4 {
+    var transformationMatrix = matrix_identity_float4x4
+    transformationMatrix[3] = [tx, ty, tz, 1]
+    return transformationMatrix * origin
+}
 
-        float3x3(rows:[simd_float3(0,    -1,    0),
-        simd_float3(1,    0,    0),
-        simd_float3(0,    0,    1)]),
+private func test3DMatchingScanners() {
+    let input = """
+    --- scanner 0 ---
+    404,-588,-901
+    528,-643,409
+    -838,591,734
+    390,-675,-793
+    -537,-823,-458
+    -485,-357,347
+    -345,-311,381
+    -661,-816,-575
+    -876,649,763
+    -618,-824,-621
+    553,345,-567
+    474,580,667
+    -447,-329,318
+    -584,868,-557
+    544,-627,-890
+    564,392,-477
+    455,729,728
+    -892,524,684
+    -689,845,-530
+    423,-701,434
+    7,-33,-71
+    630,319,-379
+    443,580,662
+    -789,900,-551
+    459,-707,401
 
-        float3x3(rows:[simd_float3(0,    0,    1),
-        simd_float3(1,    0,    0),
-        simd_float3(0,    1,    0)]),
+    --- scanner 1 ---
+    686,422,578
+    605,423,415
+    515,917,-361
+    -336,658,858
+    95,138,22
+    -476,619,847
+    -340,-569,-846
+    567,-361,727
+    -460,603,-452
+    669,-402,600
+    729,430,532
+    -500,-761,534
+    -322,571,750
+    -466,-666,-811
+    -429,-592,574
+    -355,545,-477
+    703,-491,-529
+    -328,-685,520
+    413,935,-424
+    -391,539,-444
+    586,-435,557
+    -364,-763,-893
+    807,-499,-711
+    755,-354,-619
+    553,889,-390
 
-        float3x3(rows:[simd_float3(0,    1,    0),
-        simd_float3(1,    0,    0),
-        simd_float3(0,    0,    -1)]),
+    --- scanner 2 ---
+    649,640,665
+    682,-795,504
+    -784,533,-524
+    -644,584,-595
+    -588,-843,648
+    -30,6,44
+    -674,560,763
+    500,723,-460
+    609,671,-379
+    -555,-800,653
+    -675,-892,-343
+    697,-426,-610
+    578,704,681
+    493,664,-388
+    -671,-858,530
+    -667,343,800
+    571,-461,-707
+    -138,-166,112
+    -889,563,-600
+    646,-828,498
+    640,759,510
+    -630,509,768
+    -681,-892,-333
+    673,-379,-804
+    -742,-814,-386
+    577,-820,562
 
-        float3x3(rows:[simd_float3(0,    0,    -1),
-        simd_float3(1,    0,    0),
-        simd_float3(0,    -1,    0)]),
+    --- scanner 3 ---
+    -589,542,597
+    605,-692,669
+    -500,565,-823
+    -660,373,557
+    -458,-679,-417
+    -488,449,543
+    -626,468,-788
+    338,-750,-386
+    528,-832,-391
+    562,-778,733
+    -938,-730,414
+    543,643,-506
+    -524,371,-870
+    407,773,750
+    -104,29,83
+    378,-903,-323
+    -778,-728,485
+    426,699,580
+    -438,-605,-362
+    -469,-447,-387
+    509,732,623
+    647,635,-688
+    -868,-804,481
+    614,-800,639
+    595,780,-596
 
-        float3x3(rows:[simd_float3(-1,    0,    0),
-        simd_float3(0,    -1,    0),
-        simd_float3(0,    0,    1)]),
+    --- scanner 4 ---
+    727,592,562
+    -293,-554,779
+    441,611,-461
+    -714,465,-776
+    -743,427,-804
+    -660,-479,-426
+    832,-632,460
+    927,-485,-438
+    408,393,-506
+    466,436,-512
+    110,16,151
+    -258,-428,682
+    -393,719,612
+    -211,-452,876
+    808,-476,-593
+    -575,615,604
+    -485,667,467
+    -680,325,-822
+    -627,-443,-432
+    872,-547,-609
+    833,512,582
+    807,604,487
+    839,-516,451
+    891,-625,532
+    -652,-548,-490
+    30,-46,-14
+    """
+    let scannersText = input.components(separatedBy: "\n\n")
 
-        float3x3(rows:[simd_float3(-1,    0,    0),
-        simd_float3(0,    0,    -1),
-        simd_float3(0,    -1,    0)]),
+    var scanners: [[simd_float4]] = []
 
-        float3x3(rows:[simd_float3(-1,    0,    0),
-        simd_float3(0,    1,    0),
-        simd_float3(0,    0,    -1)]),
-
-        float3x3(rows:[simd_float3(-1,    0,    0),
-        simd_float3(0,    0,    1),
-        simd_float3(0,    1,    0)]),
-
-        float3x3(rows:[simd_float3(0,    1,    0),
-        simd_float3(-1,    0,    0),
-        simd_float3(0,    0,    1)]),
-
-        float3x3(rows:[simd_float3(0,    0,    1),
-        simd_float3(-1,    0,    0),
-        simd_float3(0,    -1,    0)]),
-
-        float3x3(rows:[simd_float3(0,    -1,    0),
-        simd_float3(-1,    0,    0),
-        simd_float3(0,    0,    -1)]),
-
-        float3x3(rows:[simd_float3(0,    0,    -1),
-        simd_float3(-1,    0,    0),
-        simd_float3(0,    1,    0)]),
-
-        float3x3(rows:[simd_float3(0,    0,    -1),
-        simd_float3(0,    1,    0),
-        simd_float3(1,    0,    0)]),
-
-        float3x3(rows:[simd_float3(0,    1,    0),
-        simd_float3(0,    0,    1),
-        simd_float3(1,    0,    0)]),
-
-        float3x3(rows:[simd_float3(0,    0,    1),
-        simd_float3(0,    -1,    0),
-        simd_float3(1,    0,    0)]),
-
-        float3x3(rows:[simd_float3(0,    -1,    0),
-        simd_float3(0,    0,    -1),
-        simd_float3(1,    0,    0)]),
-
-        float3x3(rows:[simd_float3(0,    0,    -1),
-        simd_float3(0,    -1,    0),
-        simd_float3(-1,    0,    0)]),
-
-        float3x3(rows:[simd_float3(0,    -1,    0),
-        simd_float3(0,    0,    1),
-        simd_float3(-1,    0,    0)]),
-
-        float3x3(rows:[simd_float3(0,    0,    1),
-        simd_float3(0,    1,    0),
-        simd_float3(-1,    0,    0)]),
-
-        float3x3(rows:[simd_float3(0,    1,    0),
-        simd_float3(0,    0,    -1),
-        simd_float3(-1,    0,    0)])
-    ]
-
-    let input: [String] = try readInput(fromTestFile: false, separator: "\n\n")
-    let scanners: [[simd_float4]] = input.map({
-        let lineSplit = $0.split(separator: "\n")
-        return lineSplit[1..<lineSplit.count].map({ coordLine in
-            let coordSplit = coordLine.split(separator: ",")
+    for text in scannersText {
+        let coordsAsStrings = text.split(separator: "\n").suffix(from: 1)
+        scanners.append(coordsAsStrings.map({ coord in
+            let coordSplit = coord.split(separator: ",")
             return simd_float4(x: Float(coordSplit[0])!, y: Float(coordSplit[1])!, z: Float(coordSplit[2])!, w: 1)
-        })
-    })
-//    print(rotationMatrices)
-//    let secondRotation = rotationMatrices[1]
-//    for p in scanners[0] {
-//        print(secondRotation * p.xyz)
-//    }
-//    assert(false)
-    for r in rotationMatrices {
-        let originalRotated = scanners[0].map({ (r * $0.xyz).in4d })
-        let toCheckRotated = scanners[1].map({ (r * $0.xyz).in4d })
-        var allPairs: [(simd_float4, simd_float4)] = []
+        }))
+    }
 
-        for a in toCheckRotated {
-            for b in originalRotated {
-                allPairs.append((a, b))
-            }
-        }
-        for p in allPairs {
-    //        print(p)
-            let translation = makeTranslationMatrix(tx: p.1.x - p.0.x, ty: p.1.y - p.0.y, tz: p.1.z - p.0.z)
-    //        print(translation)
-            // Translate each coord in the non-zero set
-            var matches = 0
-            for coord in toCheckRotated {
-                // Translate the coord
-                let translated = translation * coord
-//                print(coord, translated)
-                if originalRotated.contains(translated) {
-                    matches += 1
-                }
-            }
-            print(matches)
+    print(scanners)
+
+    let baseScanner = scanners[0]
+    let scannerToCheck = scanners[1]
+
+    // We need to go through all rotations and check whether the rotation ends up matching the coords
+    let scannerToCheckRotated = getAllRotatedCoords(using: RotationMatrixStore.allRotationMatrices[10], on: scannerToCheck)
+//    for rotationMatrix in RotationMatrixStore.allRotationMatrices {
+//
+//    }
+    // Want to know if scanner 1 overlaps scanner 0
+    // Need to get all pairs of coordinates [(a, b)] where a is scanner 1 coord and b is scanner 0 coord
+
+    var pairs: [(simd_float4, simd_float4)] = []
+    for a in scannerToCheckRotated {
+        for b in baseScanner {
+            pairs.append((a, b))
         }
     }
 
-    assert(false)
+    for p in pairs {
+        print("Pair: \(p)")
+        let xDiff = p.1.x - p.0.x
+        let yDiff = p.1.y - p.0.y
+        let zDiff = p.1.z - p.0.z
+        print("\tDifference: \(xDiff) \(yDiff) \(zDiff)")
 
+        var matchingCoordCount = 0
+        var matchingCoords: [simd_float4] = []
+        // For each coordinate in the non-base scanner, translate it by the difference.
+        // If the translated coord is in the base scanner's coord set, increase the count
+        for nonBaseCoord in scannerToCheckRotated {
+            let translated = perform3DTranslation(origin: nonBaseCoord, tx: xDiff, ty: yDiff, tz: zDiff)
+            print("\t\tTranslated: \(nonBaseCoord) -> \(translated)")
+            if baseScanner.contains(translated) {
+                matchingCoordCount += 1
+                matchingCoords.append(scannerToCheck[scannerToCheckRotated.firstIndex(of: nonBaseCoord)!])
+            }
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    let originalCoordList = scanners[0]
-//    let toCheckCoordList = scanners[1]
-//
-//    for rotationMatrix in rotationMatrices {
-////        print("\n")
-////        print(rotationMatrix)
-////        originalCoordList.forEach({ print($0 * rotationMatrix) })
-//
-//        let originalRotatedCoords = originalCoordList.map({ $0 * rotationMatrix })
-//        let toCheckRotatedCoords = toCheckCoordList.map({ $0 * rotationMatrix })
-//
-//        for aCoord in toCheckRotatedCoords {
-//            for bCoord in originalRotatedCoords {
-//                let translationMatrix = makeTranslationMatrix(tx: bCoord.x - aCoord.x, ty: bCoord.y - aCoord.y, tz: bCoord.z - aCoord.z)
-//                // Translate all the toCheck coords by the matrix, check if it's in original rotated
-//                var matched = 0
-//                for coord in toCheckRotatedCoords {
-//                    // translate it
-//                    let translated = coord * translationMatrix
-//                    if originalRotatedCoords.contains(translated) {
-//                        matched += 1
-//                    }
-//                }
-//
-//                if matched >= 12 {
-//                    print("Done with rotation: \(rotationMatrix) \(aCoord) \(bCoord)")
-//                    assert(false)
-//                }
-//
-//            }
-//        }
-//    }
+//        print("\tOverlapping coord count: \(matchingCoordCount)")
+        if matchingCoordCount >= 12 {
+            for m in matchingCoords { print(m) }
+            let nonBaseScannerPosition = simd_float4(x: xDiff, y: yDiff, z: zDiff, w: 1)
+            print("Non-base scanner pos relative to base scanner: ", nonBaseScannerPosition)
+            assert(false)
+        }
+    }
 }
 
-private func makeTranslationMatrix(tx: Float, ty: Float) -> simd_float3x3 {
-    var matrix = matrix_identity_float3x3
+private func test2DMatchingScanners() {
+    let input = """
+    --- scanner 0 ---
+    0,2
+    4,1
+    3,3
 
-    matrix[2, 0] = tx
-    matrix[2, 1] = ty
+    --- scanner 1 ---
+    -1,-1
+    -5,0
+    -2,1
+    """
+    let scannersText = input.components(separatedBy: "\n\n")
 
-    return matrix
+    var scanners: [[simd_float3]] = []
+
+    for text in scannersText {
+        let coordsAsStrings = text.split(separator: "\n").suffix(from: 1)
+        scanners.append(coordsAsStrings.map({ coord in
+            let coordSplit = coord.split(separator: ",")
+            return simd_float3(x: Float(coordSplit[0])!, y: Float(coordSplit[1])!, z: 1)
+        }))
+    }
+
+    print(scanners)
+
+    let baseScanner = scanners[0]
+    let scannerToCheck = scanners[1]
+
+    // Want to know if scanner 1 overlaps scanner 0
+    // Need to get all pairs of coordinates [(a, b)] where a is scanner 1 coord and b is scanner 0 coord
+
+    var pairs: [(simd_float3, simd_float3)] = []
+    for a in scannerToCheck {
+        for b in baseScanner {
+            pairs.append((a, b))
+        }
+    }
+
+    for p in pairs {
+        print("Pair: \(p)")
+        let xDiff = p.1.x - p.0.x
+        let yDiff = p.1.y - p.0.y
+        print("\tDifference: \(xDiff) \(yDiff)")
+
+        var matchingCoordCount = 0
+        // For each coordinate in the non-base scanner, translate it by the difference.
+        // If the translated coord is in the base scanner's coord set, increase the count
+        for nonBaseCoord in scannerToCheck {
+            let translated = perform2DTranslation(origin: nonBaseCoord, tx: xDiff, ty: yDiff)
+            print("\t\tTranslated: \(nonBaseCoord) -> \(translated)")
+            if baseScanner.contains(translated) {
+                matchingCoordCount += 1
+            }
+        }
+
+        print("\tOverlapping coord count: \(matchingCoordCount)")
+    }
 }
 
-private func makeTranslationMatrix(tx: Float, ty: Float, tz: Float) -> simd_float4x4 {
-    var matrix = matrix_identity_float4x4
+private func testTranslationsAndRotations() {
+    let example2D = simd_float3(x: 2, y: 3, z: 1)
+    print(perform2DTranslation(origin: example2D, tx: 3, ty: -5))
 
-    matrix[3, 0] = tx
-    matrix[3, 1] = ty
-    matrix[3, 2] = tz
+    let example3D = simd_float4(x: 3, y: 5, z: 7, w: 1)
+    print(perform3DTranslation(origin: example3D, tx: 1, ty: 2, tz: 3))
 
-    return matrix
+    let coordsToRotate: [simd_float4] = [
+        simd_float4(x: -1, y: -1, z: 1, w:1),
+        simd_float4(x: 5, y: 6, z: -4, w:1),
+        simd_float4(x: 8, y: 0, z: 7, w:1)
+    ]
+    for rotation in RotationMatrixStore.allRotationMatrices {
+        print("\n")
+        for c in getAllRotatedCoords(using: rotation, on: coordsToRotate) {
+            print(c)
+        }
+    }
 }
-
-//private func scannersOverlap(originalCoordSet: Set<simd_float3>, translatableCoordSet: Set<simd_float3>, rotation: float3x3, threshold: Int) -> [simd_float3]? {
-//    let rotatedOriginalCoords = originalCoordSet.map({ $0 * rotation })
-//    let rotatedTranslatableCoords = translatableCoordSet.map({ $0 * rotation })
-//
-//    for aCoord in rotatedTranslatableCoords {
-//        for bCoord in rotatedOriginalCoords {
-//            let translationMatrix = makeTranslationMatrix(tx: bCoord.x - aCoord.x, ty: bCoord.y - aCoord.y, tz: bCoord.z - aCoord.z)
-//            var matched: [simd_float3] = []
-//            for coordToTranslate in rotatedTranslatableCoords {
-//                let translatedVector = translationMatrix * coordToTranslate
-//                if rotatedOriginalCoords.contains(translatedVector) {
-////                    print("match")
-//                    matched.append(translatedVector)
-//                }
-//            }
-////            print
-//            print(matched.count)
-//            if matched.count >= threshold {
-//                return matched
-//            }
-//        }
-//    }
-//
-//    return nil
-//}
 
 try main()
