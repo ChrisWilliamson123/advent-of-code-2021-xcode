ab5ae508f56d2d95deed2275e96af24ed75ac650
Chris Williamson <chris.williamson@bbc.co.uk>
2021-12-21 13:24:18 +0000

    Finished refactoring day 19

--- AdventOfCode/2021/day19/main.swift
+++ AdventOfCode/2021/day19/main.swift
@@ -2,102 +2,90 @@ import simd
 
 func main() throws {
     let input: [String] = try readInput(fromTestFile: false, separator: "\n\n")
-    test3DMatchingScanners(input: input)
-}
+    let scanners = buildInitialScanners(from: input)
 
-private func getAllRotatedCoords(using rotation: float4x4, on coords: Set<simd_float4>) -> Set<simd_float4> {
-    coords.reduce(into: [], { $0.insert(rotation * $1) })
-}
+    /// Stores the beacons for each scanner (translated into beacon 0's coordinate space)
+    var scannerToBeaconsMap: [Int: Set<simd_float4>] = [0: scanners[0]]
 
-private func perform3DTranslation(origin: simd_float4, tx: Float, ty: Float, tz: Float) -> simd_float4 {
-    var transformationMatrix = matrix_identity_float4x4
-    transformationMatrix[3] = [tx, ty, tz, 1]
-    return transformationMatrix * origin
-}
-
-private func test3DMatchingScanners(input: [String]) {
-    let scannersText = input
-
-    var scanners: [[simd_float4]] = []
-
-    for text in scannersText {
-        let coordsAsStrings = text.split(separator: "\n").suffix(from: 1)
-        scanners.append(coordsAsStrings.map({ coord in
-            let coordSplit = coord.split(separator: ",")
-            return simd_float4(x: Float(coordSplit[0])!, y: Float(coordSplit[1])!, z: Float(coordSplit[2])!, w: 1)
-        }))
-    }
-
-    var allBeacons: Set<simd_float4> = Set(scanners[0])
-    var scannerCoordsInZeroSpace: [Int: Set<simd_float4>] = [0: allBeacons]
-    var done: Set<Int> = []
+    var scannersCheckedForOverlaps: Set<Int> = []
     var scannerPositions: Set<simd_float4> = []
 
-    while scannerCoordsInZeroSpace.count != scanners.count {
-        let next = scannerCoordsInZeroSpace.first(where: { !done.contains($0.key) })!
-        print("Attempting to get overlapping scanner for base scanner \(next.key)")
-        for nbIndex in 0..<scanners.count where nbIndex != next.key && !scannerCoordsInZeroSpace.keys.contains(nbIndex) {
-            if let matching = getMatchingBeaconsBetween(baseScanner: next.value, nonBaseScanner: Set(scanners[nbIndex])) {
-                scannerCoordsInZeroSpace[nbIndex] = matching.allTransformedNonBaseCoordsIntoBaseSpace
-                for c in matching.allTransformedNonBaseCoordsIntoBaseSpace {
-                    allBeacons.insert(c)
-                }
+    let allScannersChecked = { return scannerToBeaconsMap.count == scanners.count }
 
-                print("Scanner \(nbIndex) is at \(matching.scannerPosition) to 0")
-                scannerPositions.insert(matching.scannerPosition)
-            } else {
-                print("Scanner \(nbIndex) does not overlap with scanner \(next.key)")
-            }
-            done.insert(next.key)
+    while !allScannersChecked() {
+        /// Will get a random scanner that hasn't been checked yet
+        let nextScannerToCheck = scannerToBeaconsMap.first(where: { !scannersCheckedForOverlaps.contains($0.key) })!
+
+        /// Loop through scanners that are not the scanner being checked and that aren't scanners that have already been resolved
+        for otherScanner in 0..<scanners.count where otherScanner != nextScannerToCheck.key && !scannerToBeaconsMap.keys.contains(otherScanner) {
+            defer { scannersCheckedForOverlaps.insert(nextScannerToCheck.key) }
+
+            guard let matching = getMatchingBeaconsBetween(baseScanner: nextScannerToCheck.value, nonBaseScanner: Set(scanners[otherScanner])) else { continue }
+
+            scannerToBeaconsMap[otherScanner] = matching.allTransformedNonBaseCoordsIntoBaseSpace
+            scannerPositions.insert(matching.scannerPosition)
+
+            print("Scanner \(otherScanner) is at \(matching.scannerPosition)")
         }
     }
 
+    let allBeacons = Set(scannerToBeaconsMap.values.flatMap({ $0 }))
     print("Part 1:", allBeacons.count)
 
     let scannerPairs = Array(scannerPositions).combinations(count: 2)
-    let maxDistance: Float = scannerPairs.reduce(0, { max($0, abs($1[0].x - $1[1].x) + abs($1[0].y - $1[1].y) + abs($1[0].z - $1[1].z)) })
-    print("Part 2:", Int(maxDistance))
+    let maxDistanceBetweenScanners: Float = scannerPairs.reduce(0, { max($0, abs($1[0].x - $1[1].x) + abs($1[0].y - $1[1].y) + abs($1[0].z - $1[1].z)) })
+    print("Part 2:", Int(maxDistanceBetweenScanners))
 }
 
-/// Will optionally return a translation that describes how to get the nonBaseScanner to the base
+/*
+ Will optionally return a tuple where:
+    item 0 is the relative scanners position from the base
+    item 1 is a set containing all of the relative scanner's beacons translated to the base scanner's coordinate space
+ */
 private func getMatchingBeaconsBetween(baseScanner: Set<simd_float4>, nonBaseScanner: Set<simd_float4>) -> (scannerPosition: simd_float4,
                                                                                                             allTransformedNonBaseCoordsIntoBaseSpace: Set<simd_float4>)? {
-    // We need to go through all rotations and check whether the rotation ends up matching the coords
+    // Loop through all possible orientations that a scanner can have
     for rotationMatrix in RotationMatrixStore.allRotationMatrices {
-        let scannerToCheckWithRotatedCoords = getAllRotatedCoords(using: rotationMatrix, on: nonBaseScanner)
+        let rotatedBeacons: Set<simd_float4> = nonBaseScanner.reduce(into: [], { $0.insert(rotationMatrix * $1) })
 
-        // Want to know if scanner 1 with it's rotation overlaps scanner 0
-        // Need to get all pairs of coordinates [(a, b)] where a is scanner 1 coord and b is scanner 0 coord
         var pairs: [(simd_float4, simd_float4)] = []
-        for a in scannerToCheckWithRotatedCoords {
+        for a in rotatedBeacons {
             for b in baseScanner {
                 pairs.append((a, b))
             }
         }
 
         for p in pairs {
-            let xDiff = p.1.x - p.0.x
-            let yDiff = p.1.y - p.0.y
-            let zDiff = p.1.z - p.0.z
+            var translationMatrix = matrix_identity_float4x4
+            let translationColumn = simd_float4(arrayLiteral: p.1.x - p.0.x, p.1.y - p.0.y, p.1.z - p.0.z, 1)
+            translationMatrix[3] = translationColumn
 
-            var matchingCoordCount = 0
-
-            // For each coordinate in the non-base scanner, translate it by the difference.
-            // If the translated coord is in the base scanner's coord set, increase the count
-            for nonBaseCoord in scannerToCheckWithRotatedCoords {
-                let translated = perform3DTranslation(origin: nonBaseCoord, tx: xDiff, ty: yDiff, tz: zDiff)
-                if baseScanner.contains(translated) { matchingCoordCount += 1 }
-            }
+            let matchingCoordCount = rotatedBeacons.reduce(0, { baseScanner.contains(translationMatrix * $1) ? $0 + 1 : $0 })
 
             if matchingCoordCount >= 12 {
                 var matrix = rotationMatrix
-                matrix[3] = [xDiff, yDiff, zDiff, 1]
+                matrix[3] = translationColumn
                 let allNonBaseTransformed: Set<simd_float4> = nonBaseScanner.reduce(into: [], { $0.insert(matrix * $1) })
-                return (simd_float4(x: -xDiff, y: -yDiff, z: -zDiff, w: 1), allNonBaseTransformed)
+                return (simd_float4(x: -translationColumn.x, y: -translationColumn.y, z: -translationColumn.z, w: 1), allNonBaseTransformed)
             }
         }
     }
+
     return nil
+}
+
+private func buildInitialScanners(from input: [String]) -> [Set<simd_float4>] {
+    var scanners: [Set<simd_float4>] = []
+
+    for text in input {
+        let coordsAsStrings = text.split(separator: "\n").suffix(from: 1)
+        scanners.append(Set(coordsAsStrings.map({ coord in
+            let coordSplit = coord.split(separator: ",")
+            return simd_float4(x: Float(coordSplit[0])!, y: Float(coordSplit[1])!, z: Float(coordSplit[2])!, w: 1)
+        })))
+    }
+
+    return scanners
 }
 
 try main()
