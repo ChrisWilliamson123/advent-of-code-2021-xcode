3669158b962e707d95c38cac6afc92881a4bc89b
Chris Williamson <chris.williamson@bbc.co.uk>
2021-12-27 16:59:30 +0000

    Done 2020 day 21 part one

--- AdventOfCode/2020/day21/main.swift
+++ AdventOfCode/2020/day21/main.swift
@@ -1,9 +1,44 @@
 import Foundation
 
 func main() throws {
-    let input: [String] = try readInput(fromTestFile: false)
+    let foods: [String] = try readInput(fromTestFile: false)
 
-    print(input)
+    var allergensToPotentialIngredients: [String: Set<String>] = [:]
+    var allIngredients: Set<String> = []
+    var allIngredientsCounts: [String: Int] = [:]
+
+    for f in foods {
+        let regex = Regex("(.*) \\(contains (.*)\\)")
+        let matches = regex.getMatches(in: f)
+        let ingredients = matches[0].split(separator: " ").map({ String($0) })
+        let allergens = matches[1].components(separatedBy: ", ")
+        for a in allergens {
+            if let current = allergensToPotentialIngredients[String(a)] {
+                allergensToPotentialIngredients[String(a)] = current.intersection(Set(ingredients))
+            } else {
+                allergensToPotentialIngredients[String(a)] = Set(ingredients)
+            }
+        }
+
+        allIngredients = allIngredients.union(Set(ingredients))
+        for i in ingredients {
+            allIngredientsCounts[i] = (allIngredientsCounts[i] ?? 0) + 1
+        }
+    }
+
+    print(allergensToPotentialIngredients)
+    let allergenedIngredients: Set<String> = allergensToPotentialIngredients.reduce([], { $0.union($1.value) })
+    let nonAllergenedIngredients = allIngredients.subtracting(allergenedIngredients)
+    print(nonAllergenedIngredients)
+
+    print(nonAllergenedIngredients.map({ allIngredientsCounts[$0]! }).sum())
+
+//    let singleValueItem = ingredientsToPotentialAllergens.first(where: { $0.value.count == 1 })!
+//    for (key, value) in ingredientsToPotentialAllergens where key != singleValueItem.key {
+//        ingredientsToPotentialAllergens[key] = value.filter({ $0 != singleValueItem.value[0] })
+//    }
+//
+//    print(ingredientsToPotentialAllergens)
 }
 
 try main()
