c7717fb04d72cc2a94e475060a9c5374bd1f2204
Chris Williamson <chris.williamson@bbc.co.uk>
2021-12-28 21:36:54 +0000

    2015 day 4 done

--- AdventOfCode/2015/day4/main.swift
+++ AdventOfCode/2015/day4/main.swift
@@ -1,7 +1,39 @@
 import Foundation
+import var CommonCrypto.CC_MD5_DIGEST_LENGTH
+import func CommonCrypto.CC_MD5
+import typealias CommonCrypto.CC_LONG
+
+func MD5(string: String) -> Data {
+    let length = Int(CC_MD5_DIGEST_LENGTH)
+    let messageData = string.data(using:.utf8)!
+    var digestData = Data(count: length)
+
+    _ = digestData.withUnsafeMutableBytes { digestBytes -> UInt8 in
+        messageData.withUnsafeBytes { messageBytes -> UInt8 in
+            if let messageBytesBaseAddress = messageBytes.baseAddress, let digestBytesBlindMemory = digestBytes.bindMemory(to: UInt8.self).baseAddress {
+                let messageLength = CC_LONG(messageData.count)
+                CC_MD5(messageBytesBaseAddress, messageLength, digestBytesBlindMemory)
+            }
+            return 0
+        }
+    }
+    return digestData
+}
+
+func MD5Hex(string: String) -> String {
+    let md5Data = MD5(string: string)
+    return md5Data.map { String(format: "%02hhx", $0) }.joined()
+}
 
 func main() throws {
-    let input: [Int] = try readInput(fromTestFile: true)
+    let secretKey = "yzbqklnj"
+
+    var number = 1
+    while MD5Hex(string: secretKey + String(number))[0..<6] != "000000" {
+        number += 1
+    }
+
+    print(number)
 }
 
 try main()
