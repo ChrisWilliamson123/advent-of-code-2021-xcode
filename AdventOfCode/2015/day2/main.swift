deca4032637c89b274e2fcc1ab0a31ef95b2f805
Chris Williamson <chris.williamson@bbc.co.uk>
2021-12-28 20:56:48 +0000

    2015 day 2 done

--- AdventOfCode/2015/day2/main.swift
+++ AdventOfCode/2015/day2/main.swift
@@ -1,7 +1,32 @@
 import Foundation
 
 func main() throws {
-    let input: [Int] = try readInput(fromTestFile: true)
+    let input: [String] = try readInput(fromTestFile: false)
+    let presents = input.map({ Present($0) })
+    let paperNeeded = presents.map({ $0.surfaceArea + $0.smallestSide }).sum()
+    print("Part one:", paperNeeded)
+
+    let ribbonNeeded = presents.map({ $0.ribbonRequired + $0.bowRibbonNeeded }).sum()
+    print("Part two:", ribbonNeeded)
+}
+
+struct Present {
+    let length: Int
+    let width: Int
+    let height: Int
+
+    var surfaceArea: Int { (2*length*width) + (2*width*height) + (2*height*length) }
+    var smallestSide: Int { [length, width, height].sorted()[0...1].multiply() }
+    var ribbonRequired: Int { [length, width, height].sorted()[0...1].sum() * 2 }
+    var bowRibbonNeeded: Int { length * width * height }
+
+    init(_ presentString: String) {
+        let regex = Regex("(\\d+)x(\\d+)x(\\d+)")
+        let matches = regex.getMatches(in: presentString)
+        self.length = Int(matches[0])!
+        self.width = Int(matches[1])!
+        self.height = Int(matches[2])!
+    }
 }
 
 try main()
