38693d0ba9e8bea28d23b0a0439427208ee491b5
Chris Williamson <chris.williamson@bbc.co.uk>
2021-12-30 22:22:39 +0000

    Done 2015 day 10

--- AdventOfCode/2015/day10/main.swift
+++ AdventOfCode/2015/day10/main.swift
@@ -1,7 +1,38 @@
 import Foundation
 
 func main() throws {
-    let input: [Int] = try readInput(fromTestFile: true)
+    var currentSequence: [Int] = [1,3,2,1,1,3,1,1,1,2]
+
+    for i in 0..<50 {
+        currentSequence = tick(seq: currentSequence)
+        if i == 39 {
+            print("Part one:", currentSequence.count)
+        }
+    }
+
+    print("Part two:", currentSequence.count)
+}
+
+private func tick(seq: [Int]) -> [Int] {
+    var currentNumber = seq[0]
+    var currentCount = 1
+    var index = 1
+
+    var newSequence: [Int] = []
+    while index < seq.count {
+        let value = seq[index]
+        if value != currentNumber {
+            newSequence.append(contentsOf: [currentCount, currentNumber])
+            currentCount = 1
+            currentNumber = value
+        } else {
+            currentCount += 1
+        }
+        index += 1
+    }
+    newSequence.append(contentsOf: [currentCount, currentNumber])
+
+    return newSequence
 }
 
 try main()
