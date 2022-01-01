397c858641d9d0acd67b69fb429865f12e162d2f
Chris Williamson <chris.williamson@bbc.co.uk>
2022-01-01 20:56:15 +0000

    Refactored day 14 to be more OO

--- AdventOfCode/2015/day14/main.swift
+++ AdventOfCode/2015/day14/main.swift
@@ -2,25 +2,45 @@ import Foundation
 
 func main() throws {
     let input: [String] = try readInput(fromTestFile: false)
-    var reindeers: [Reindeer] = input.map({
+    let reindeers: [Reindeer] = input.map({
         let regex = Regex("(\\w+) can fly (\\d+) km/s for (\\d+) seconds, but then must rest for (\\d+) seconds.")
         let matches = regex.getMatches(in: $0)
         return .init(name: matches[0], speed: Int(matches[1])!, flightTime: Int(matches[2])!, restTime: Int(matches[3])!)
     })
 
-    print("Part one:", reindeers.map({ $0.getDistanceTravelled(in: 2503) }).max()!)
+    print("Part one:", DistanceTravelledRace(reindeers: reindeers).getWinningScore(after: 2503))
+    print("Part two:", PointsRace(reindeers: reindeers).getWinningScore(after: 2503))
+}
 
-    for i in 1...2503 {
-        let distances = reindeers.map({ $0.getDistanceTravelled(in: i) })
-        let max = distances.max()!
-        for index in 0..<distances.count {
-            if distances[index] == max {
-                reindeers[index].score += 1
+struct DistanceTravelledRace: Race {
+    let reindeers: [Reindeer]
+
+    func getWinningScore(after time: Int) -> Int {
+        reindeers.map({ $0.getDistanceTravelled(in: time) }).max()!
+    }
+}
+
+struct PointsRace: Race {
+    let reindeers: [Reindeer]
+
+    func getWinningScore(after time: Int) -> Int {
+        var reindeers = self.reindeers
+        for i in 1...time {
+            let distances = reindeers.map({ $0.getDistanceTravelled(in: i) })
+            let max = distances.max()!
+            for index in 0..<distances.count {
+                if distances[index] == max {
+                    reindeers[index].score += 1
+                }
             }
         }
+        return reindeers.max(by: { $0.score < $1.score })!.score
     }
+}
 
-    print("Part two:", reindeers.max(by: { $0.score < $1.score })!.score)
+protocol Race {
+    var reindeers: [Reindeer] { get }
+    func getWinningScore(after time: Int) -> Int
 }
 
 struct Reindeer {
