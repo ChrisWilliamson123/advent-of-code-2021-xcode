1274e9dd85bd10e072425f1604c006177d73c2f4
Chris Williamson <chris.williamson@bbc.co.uk>
2022-01-01 18:18:26 +0000

    Done 2015 day 12, used jq to remove objects with a key/value with "red".

--- AdventOfCode/2015/day12/main.swift
+++ AdventOfCode/2015/day12/main.swift
@@ -1,7 +1,25 @@
 import Foundation
 
 func main() throws {
-    let input: [Int] = try readInput(fromTestFile: true)
+    let inputJSON: String = try readInput(fromTestFile: false)[0]
+
+    let numbersSum = matches(for: "(-?\\d+)", in: inputJSON).map({ Int($0)! }).sum()
+    print(numbersSum)
+}
+
+private func matches(for regex: String, in text: String) -> [String] {
+
+    do {
+        let regex = try NSRegularExpression(pattern: regex)
+        let results = regex.matches(in: text,
+                                    range: NSRange(text.startIndex..., in: text))
+        return results.map {
+            String(text[Range($0.range, in: text)!])
+        }
+    } catch let error {
+        print("invalid regex: \(error.localizedDescription)")
+        return []
+    }
 }
 
 try main()
