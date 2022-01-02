6a9464cf2c4736386510bfa1d91b6e2b86eb82b4
Chris Williamson <chris.williamson@bbc.co.uk>
2022-01-02 16:11:03 +0000

    Done 2015 day 17

--- AdventOfCode/2015/day17/main.swift
+++ AdventOfCode/2015/day17/main.swift
@@ -1,7 +1,37 @@
 import Foundation
 
 func main() throws {
-    let input: [Int] = try readInput(fromTestFile: true)
+    let input: [String] = try readInput(fromTestFile: false)
+    let containers: [Container] = input.enumerated().map({ (index, inputLine) in
+        return Container(capacity: Int(inputLine)!, id: index)
+    })
+    let litresToStore: Int = 150
+    var subsets: Set<Set<Container>> = []
+    func subset_sum(numbers: [Container], target: Int, partial: [Container] = []) {
+        let sum = partial.map({$0.capacity}).sum()
+        if sum == target {
+            subsets.insert(Set(partial))
+        }
+        if sum >= target {
+            return
+        }
+        for i in 0..<numbers.count {
+            let n = numbers[i]
+            let remaining = numbers[i+1..<numbers.count]
+            subset_sum(numbers: Array(remaining), target: target, partial: partial + [n])
+        }
+    }
+
+    subset_sum(numbers: containers, target: litresToStore)
+    print("Part one:", subsets.count)
+    let minimumNumberOfContainersUsed = subsets.map({ $0.count }).min()!
+    let subsetsWithMinUsed = subsets.filter({ $0.count == minimumNumberOfContainersUsed }).count
+    print("Part two:", subsetsWithMinUsed)
+}
+
+struct Container: Hashable {
+    let capacity: Int
+    let id: Int
 }
 
 try main()
