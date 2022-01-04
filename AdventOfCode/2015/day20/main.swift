59ee1b45ff47ba1eff6424482a2f72495117ffc0
Chris Williamson <chris.williamson@bbc.co.uk>
2022-01-04 10:29:58 +0000

    Printing part one and two for 2015 day 20

--- AdventOfCode/2015/day20/main.swift
+++ AdventOfCode/2015/day20/main.swift
@@ -4,13 +4,20 @@ func main() throws {
     let factorsInput: [String] = try readInput(fileName: "factors.txt")
     var index = 1
     let factors: [Int: Set<Int>] = factorsInput.reduce(into: [:], { $0[index] = Set($1.split(separator: ",").map({Int($0)!})); index += 1 })
+    print("Built factors dict")
+
     var house = 1
+    while factors[house]!.reduce(0, +) * 10 < 34000000 {
+        house += 1
+    }
+    print("Part one:", house)
+
+    house = 1
     while factors[house]!.filter({ house / $0 <= 50 }).reduce(0, +) * 11 < 34000000 {
-        if house % 10000 == 0 { print(house) }
         house += 1
     }
 
-    print(house)
+    print("Part two:", house)
 }
 
 try main()
