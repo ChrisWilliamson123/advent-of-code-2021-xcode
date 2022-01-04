a612ebd18b1a0f2b1f9e831f0ff7b50de56677e7
Chris Williamson <chris.williamson@bbc.co.uk>
2022-01-04 10:25:51 +0000

    Done 2015 day 20

--- AdventOfCode/2015/day20/main.swift
+++ AdventOfCode/2015/day20/main.swift
@@ -1,7 +1,16 @@
 import Foundation
 
 func main() throws {
-    let input: [Int] = try readInput(fromTestFile: true)
+    let factorsInput: [String] = try readInput(fileName: "factors.txt")
+    var index = 1
+    let factors: [Int: Set<Int>] = factorsInput.reduce(into: [:], { $0[index] = Set($1.split(separator: ",").map({Int($0)!})); index += 1 })
+    var house = 1
+    while factors[house]!.filter({ house / $0 <= 50 }).reduce(0, +) * 11 < 34000000 {
+        if house % 10000 == 0 { print(house) }
+        house += 1
+    }
+
+    print(house)
 }
 
 try main()
