f58150846f0c4c4170c05e8fd555f9592437a16a
Chris Williamson <chris.williamson@bbc.co.uk>
2022-01-05 21:14:28 +0000

    Done 2015 day 24, mainly by hand

--- AdventOfCode/2015/day24/main.swift
+++ AdventOfCode/2015/day24/main.swift
@@ -1,7 +1,19 @@
 import Foundation
 
 func main() throws {
-    let input: [Int] = try readInput(fromTestFile: true)
+    let packageWeights: [Int] = try readInput(fromTestFile: false)
+    let packageCount = packageWeights.count
+
+    let groupWeight: Int = packageWeights.sum() / 4
+
+    print(groupWeight, packageCount)
+
+//    let smallest = [113, 109, 107, 101, 89, 1]
+    let smallest = [113, 109, 107, 61]
+    let partOne = smallest.multiply()
+    print(partOne)
+
+
 }
 
 try main()
