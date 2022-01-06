379e87e980c1f68c118be193e64d06736686a029
Chris Williamson <chris.williamson@bbc.co.uk>
2022-01-06 12:17:30 +0000

    Done 2016 day 6

--- AdventOfCode/2016/day6/main.swift
+++ AdventOfCode/2016/day6/main.swift
@@ -2,6 +2,12 @@ import Foundation
 
 func main() throws {
     let input: [String] = try readInput(fromTestFile: false)
+
+    var counts: [[Character: Int]] = Array(repeating: [:], count: input[0].count)
+    for l in input { for i in 0..<l.count { counts[i][l[i], default: 0] += 1 } }
+
+    print("Part one:", String(counts.map({ $0.max(by: { $0.value < $1.value })!.key })))
+    print("Part two:", String(counts.map({ $0.min(by: { $0.value < $1.value })!.key })))
 }
 
 try main()
