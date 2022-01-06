67a1ba0aa4f97e1403e34fc55aaf5023e5288b5e
Chris Williamson <chris.williamson@bbc.co.uk>
2022-01-06 09:30:09 +0000

    Done 2015 day 25.

--- AdventOfCode/2015/day25/main.swift
+++ AdventOfCode/2015/day25/main.swift
@@ -1,7 +1,27 @@
 import Foundation
 
 func main() throws {
-    let input: [Int] = try readInput(fromTestFile: true)
+    var last = 20151125
+    var numbers: [Int] = [last]
+    for i in 0..<25000000 {
+        if i % 1000000 == 0 {
+            print(i)
+        }
+        let next = (last * 252533) % 33554393
+        numbers.append(next)
+        last = next
+    }
+
+    print(numbers[getIndex(x: 3074, y: 2980)])
+//    print(getIndex(x: 0, y: 0))
+//
+//    print(getIndex(x: 3, y: 1))
+}
+
+private func getIndex(x: Int, y: Int) -> Int {
+    let n = x + y
+    let base = (n*(n+1))/2
+    return base + x
 }
 
 try main()
