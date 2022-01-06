567b69ad36c4f362103b8c4e89e893c4989ea601
Chris Williamson <chris.williamson@bbc.co.uk>
2022-01-06 10:25:46 +0000

    Done 2016 day 1.

---
+++ AdventOfCode/helpers/Extensions/RangeReplaceableCollection+Extensions.swift
@@ -0,0 +1,25 @@
+extension RangeReplaceableCollection {
+    func rotatingLeft(positions: Int) -> SubSequence {
+        let index = self.index(startIndex, offsetBy: positions, limitedBy: endIndex) ?? endIndex
+        return self[index...] + self[..<index]
+    }
+    mutating func rotateLeft(positions: Int) {
+        let index = self.index(startIndex, offsetBy: positions, limitedBy: endIndex) ?? endIndex
+        let slice = self[..<index]
+        removeSubrange(..<index)
+        insert(contentsOf: slice, at: endIndex)
+    }
+}
+
+extension RangeReplaceableCollection {
+    func rotatingRight(positions: Int) -> SubSequence {
+        let index = self.index(endIndex, offsetBy: -positions, limitedBy: startIndex) ?? startIndex
+        return self[index...] + self[..<index]
+    }
+    mutating func rotateRight(positions: Int) {
+        let index = self.index(endIndex, offsetBy: -positions, limitedBy: startIndex) ?? startIndex
+        let slice = self[index...]
+        removeSubrange(index...)
+        insert(contentsOf: slice, at: startIndex)
+    }
+}
