624d9a00c501cff95f0d8487a3abe17c66631004
Chris Williamson <chris.williamson@bbc.co.uk>
2022-01-07 17:32:07 +0000

    Done 2016 day 9

--- AdventOfCode/2016/day9/main.swift
+++ AdventOfCode/2016/day9/main.swift
@@ -1,7 +1,36 @@
 import Foundation
 
 func main() throws {
-    let input: [String] = try readInput(fromTestFile: false)
+    let input: String = try readInput(fromTestFile: false)[0]
+
+    func expand(input: String, count: Int) -> Int {
+        if !input.contains("(") { return input.count * count }
+
+        // Else, we need to do more expansions
+        var expandedStringLength = 0
+        var index = 0
+        while index < input.count {
+            let nextChar = input[index]
+            guard nextChar == "(" else { index += 1; expandedStringLength += 1; continue }
+
+            var markerIndex = index + 1
+            while input[markerIndex] != ")" { markerIndex += 1 }
+            let nextMarker = input[index...markerIndex]
+
+            let split = Regex("\\((\\d+)x(\\d+)\\)").getMatches(in: nextMarker)
+            let decompressionLength = Int(split[0])!
+            let decompressionMultiplier = Int(split[1])!
+
+            let toReplace = input[markerIndex+1...markerIndex+decompressionLength]
+            let expansion = expand(input: toReplace, count: decompressionMultiplier)
+            expandedStringLength += expansion
+            index += nextMarker.count + decompressionLength
+        }
+
+        return expandedStringLength * count
+    }
+
+    print(expand(input: input, count: 1))
 }
 
 try main()
--- AdventOfCode/helpers/Extensions/String+Extensions.swift
+++ AdventOfCode/helpers/Extensions/String+Extensions.swift
@@ -33,7 +33,7 @@ extension String {
                                             upper: min(length, max(0, r.upperBound))))
         let start = index(startIndex, offsetBy: range.lowerBound)
         let end = index(start, offsetBy: range.upperBound - range.lowerBound)
-        return String(self[start ..< end])
+        return String(self[start ... end])
     }
 
     var characterCounts: [Character: Int] {
