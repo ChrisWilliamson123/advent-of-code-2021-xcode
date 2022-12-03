d3bea41e0b26f570f14315045d4fd54faca8bb0f
ChrisWilliamson123 <cjwilliamson109@gmail.com>
2022-12-03 16:34:49 +0000

    main: Refactored day 3

--- AdventOfCode/2022/day3/main.swift
+++ AdventOfCode/2022/day3/main.swift
@@ -1,38 +1,23 @@
 import Foundation
-//import Collections
 
 func main() throws {
     let rucksacks: [String] = try readInput(fromTestFile: false)
+    let part1 = rucksacks.reduce(0) { $0 + getDuplicateValue(in: $1) }
+    print(part1)
 
-    var sum = 0
-    for rucksack in rucksacks {
-        let asciiValues = rucksack.map { String($0).asciiNormalised }
-        let firstHalf = asciiValues[0..<asciiValues.count/2]
-        let secondHalf = asciiValues[asciiValues.count/2..<asciiValues.count]
-        let duplicate = firstHalf.first(where: { secondHalf.contains($0) })
-        duplicate.map { sum += $0 }
-    }
-    print(sum)
-
-    sum = 0
-
-    for groupIndex in stride(from: 0, to: rucksacks.count-1, by: 3) {
-        let rucksackGroups = [rucksacks[groupIndex], rucksacks[groupIndex+1], rucksacks[groupIndex+2]]
-        let common = rucksackGroups[0].first(where: { rucksackGroups[1].contains($0) && rucksackGroups[2].contains($0) })
-        common.map { sum += String($0).asciiNormalised }
-    }
-
-    print(sum)
+    let part2 = stride(from: 0, to: rucksacks.count-1, by: 3).reduce(0, { $0 + getCommonValue(in: [ rucksacks[$1], rucksacks[$1+1], rucksacks[$1+2] ]) })
+    print(part2)
 }
 
-extension StringProtocol {
-    var asciiNormalised: Int {
-        let ascii = asciiValue
-        if self.first!.isUppercase {
-            return ascii - 38
-        }
-        return ascii - 96
-    }
+func getDuplicateValue(in rucksack: String) -> Int {
+    let asciiValues = rucksack.map { String($0).asciiNormalised }
+    let firstHalf = asciiValues[0..<asciiValues.count/2]
+    let secondHalf = asciiValues[asciiValues.count/2..<asciiValues.count]
+    return firstHalf.first(where: { secondHalf.contains($0) })!
+}
+
+func getCommonValue(in rucksacks: [String]) -> Int {
+    String(rucksacks[0].first(where: { rucksacks[1].contains($0) && rucksacks[2].contains($0) })!).asciiNormalised
 }
 
 try main()
--- AdventOfCode/helpers/Extensions/StringProtocol+Extensions.swift
+++ AdventOfCode/helpers/Extensions/StringProtocol+Extensions.swift
@@ -1,4 +1,11 @@
 extension StringProtocol {
     var asciiValues: [Int] { compactMap(\.asciiValue).map { Int($0) } }
     var asciiValue: Int { asciiValues.first! }
+    var asciiNormalised: Int {
+        let ascii = asciiValue
+        if self.first!.isUppercase {
+            return ascii - 38
+        }
+        return ascii - 96
+    }
 }
