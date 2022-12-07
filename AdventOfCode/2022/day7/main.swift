74b00a2c00b0946bdb56d19b9942d261e732fcb3
ChrisWilliamson123 <cjwilliamson109@gmail.com>
2022-12-07 14:17:49 +0000

    main: Done some day 7 refactors.

--- AdventOfCode/2022/day7/main.swift
+++ AdventOfCode/2022/day7/main.swift
@@ -1,64 +1,52 @@
 import Foundation
 
 enum Command {
+    case upDir
     case changeDir(name: String)
     case list
 
     static func initialise(string: String) -> Command {
-        let raw = string.replacingOccurrences(of: "$ ", with: "")
-        if raw.starts(with: "cd") {
-            return .changeDir(name: String(raw.split(separator: " ")[1]))
-        } else {
-            return .list
+        if string.contains("..") { return .upDir }
+        if string.contains("cd") {
+            return .changeDir(name: String(string.split(separator: " ").last!))
         }
+        return .list
     }
 }
 
 func main() throws {
     let input: [String] = try readInput(fromTestFile: false)
 
-    var topDir = Directory(name: "/")
-    var currentDirectory: Directory = topDir
+    var currentDirectory: Directory = Directory(name: "/")
     var lineIndex = 1
     var directories: [Directory] = [currentDirectory]
+
     while lineIndex < input.count {
         let line = input[lineIndex]
         let command = Command.initialise(string: line)
-        if case let .changeDir(name) = command {
-            if name == ".." {
-                currentDirectory = currentDirectory.parent!
-            } else {
-                if let existingDirectory = currentDirectory.contents.first(where: { $0.name == name }) as? Directory {
-                    currentDirectory = existingDirectory
-                } else {
-                    assertionFailure("cd target doesn't exists")
-                }
-            }
+        switch command {
+        case .upDir:
+            currentDirectory = currentDirectory.parent!
             lineIndex += 1
-            continue
-        } else {
+        case .changeDir(let name):
+            let existingDirectory = currentDirectory.contents.first(where: { $0.name == name })! as! Directory
+            currentDirectory = existingDirectory
+            lineIndex += 1
+        case .list:
             let endOfListOutputIndex = ((lineIndex+1..<input.count).first(where: { input[$0].starts(with: "$") }) ?? input.count) - 1
             let newDirs = currentDirectory.createContents(from: Array(input[lineIndex+1...endOfListOutputIndex]))
             directories.append(contentsOf: newDirs)
             lineIndex = endOfListOutputIndex + 1
-            continue
         }
     }
+
     let sizes = directories.map { $0.size }
-    print(sizes.reduce(0, {
-        if $1 < 100000 {
-            return $0 + $1
-        } else {
-            return $0
-        }
-    }))
-//    print(sum)
+    print(sizes.reduce(0, { $1 < 100000 ? $0 + $1 : $0 }))
 
     let unusedSpace = 70000000 - directories[0].size
     let deletionNeeded = 30000000 - unusedSpace
-    print(unusedSpace, deletionNeeded)
     let deletionCandidates = sizes.filter({ $0 >= deletionNeeded })
-    print(deletionCandidates.sorted())
+    print(deletionCandidates.min()!)
 }
 
 protocol Entity {
@@ -78,33 +66,27 @@ class File: Entity {
 
 class Directory: Entity {
     let name: String
-    var size: Int {
-        let size = contents.map({ $0.size }).sum()
-//        print("\(name) size: \(size)")
-        return size
-    }
+    var size: Int { contents.reduce(0, { $0 + $1.size }) }
     var contents: [Entity] = []
     var parent: Directory?
+    var directories: [Directory] { contents.compactMap({ $0 as? Directory }) }
 
     init(name: String) {
         self.name = name
     }
 
     func createContents(from lines: [String]) -> [Directory] {
-        var newDirs: [Directory] = []
         for l in lines {
             if l.starts(with: "dir") {
                 let dir = Directory(name: l.replacingOccurrences(of: "dir ", with: ""))
                 dir.parent = self
                 contents.append(dir)
-                newDirs.append(dir)
             } else {
-                let name = String(String(l.split(separator: " ")[1]))
-                if contents.contains(where: { $0.name == name }) { continue }
-                contents.append(File(name: name, size: Int(l.split(separator: " ")[0])!))
+                let split = l.split(separator: " ")
+                contents.append(File(name: String(split[1]), size: Int(split[0])!))
             }
         }
-        return newDirs
+        return directories
     }
 }
 
