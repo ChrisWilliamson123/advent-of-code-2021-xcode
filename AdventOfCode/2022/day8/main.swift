d150731e14e61ea74b020cdd47568279ac51ad0e
ChrisWilliamson123 <cjwilliamson109@gmail.com>
2022-12-08 10:53:23 +0000

    main: Removed inner for loop

--- AdventOfCode/2022/day8/main.swift
+++ AdventOfCode/2022/day8/main.swift
@@ -3,7 +3,7 @@ func main() throws {
     let grid = input.map { line in line.map { Int($0)! } }
 
     var visible = 0
-    var bestVD = 0
+    var maxScenicScore = 0
     for y in 1..<(grid.count-1) {
         for x in 1..<(grid.count-1) {
             let value = grid[y][x]
@@ -11,24 +11,19 @@ func main() throws {
             let nextY = (y+1..<grid.count).map { grid[$0][x] }
             let previousX: [Int] = (0..<x).map({ grid[y][$0] }).reversed()
             let nextX = (x+1..<grid.count).map { grid[y][$0] }
-
-            var viewingDistanceTotal = 1
-            for b in [previousY, previousX, nextY, nextX] {
-                if value > b.max() ?? 0 {
-                    visible += 1
-                    for b2 in [previousY, previousX, nextY, nextX] {
-                        viewingDistanceTotal *= viewingDistance(value, blockers: b2)
-                        bestVD = max(viewingDistanceTotal, bestVD)
-                    }
-                    break
-                }
+            let allDirections = [previousY, previousX, nextY, nextX]
+            
+            if allDirections.first(where: { value > $0.max() ?? 0 }) != nil {
+                visible += 1
+                let scenicScore = allDirections.map({ viewingDistance(value, blockers: $0) }).multiply()
+                maxScenicScore = max(maxScenicScore, scenicScore)
             }
         }
     }
 
     let outsideTrees = ((grid.count - 2) * 4) + 4
     print(visible + outsideTrees)
-    print(bestVD)
+    print(maxScenicScore)
 }
 
 private func viewingDistance(_ tree: Int, blockers: [Int]) -> Int {
