623a872a510659152f10389443dd670f5e9c7b23
ChrisWilliamson123 <cjwilliamson109@gmail.com>
2022-12-08 08:57:06 +0000

    main: Done day 8.

--- AdventOfCode/2022/day8/main.swift
+++ AdventOfCode/2022/day8/main.swift
@@ -1,8 +1,48 @@
 import Foundation
 
 func main() throws {
-    let input: [String] = try readInput(fromTestFile: true)
-    print(input)
+    let input: [String] = try readInput(fromTestFile: false)
+    let grid = input.map { line in line.map { Int($0)! } }
+
+    var visible = 0
+    var bestVD = 0
+    for y in 1..<(grid.count-1) {
+        for x in 1..<(grid.count-1) {
+            let value = grid[y][x]
+            let previousY: [Int] = (0..<y).map({ grid[$0][x] }).reversed()
+            let nextY = (y+1..<grid.count).map { grid[$0][x] }
+            let previousX: [Int] = (0..<x).map({ grid[y][$0] }).reversed()
+            let nextX = (x+1..<grid.count).map { grid[y][$0] }
+
+            var viewingDistanceTotal = 1
+            for b in [previousY, previousX, nextY, nextX] {
+                if treeIsVisible(value, blockers: b) {
+                    visible += 1
+                    for b2 in [previousY, previousX, nextY, nextX] {
+                        viewingDistanceTotal *= viewingDistance(value, blockers: b2)
+                        bestVD = max(viewingDistanceTotal, bestVD)
+                    }
+                    break
+                }
+            }
+        }
+    }
+
+    let outsideTrees = ((grid.count - 2) * 4) + 4
+    print(visible + outsideTrees)
+    print(bestVD)
+}
+
+private func treeIsVisible(_ tree: Int, blockers: [Int]) -> Bool {
+    !blockers.contains(where: { $0 >= tree })
+}
+
+private func viewingDistance(_ tree: Int, blockers: [Int]) -> Int {
+    if blockers.count == 0 { return 0 }
+    if let firstBlockerIndex = blockers.firstIndex(where: { $0 >= tree }) {
+        return firstBlockerIndex + 1
+    }
+    return blockers.count
 }
 
 try main()
