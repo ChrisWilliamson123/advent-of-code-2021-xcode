23be1d02c652cc2ffdfe24babd0bab4090314cb0
ChrisWilliamson123 <cjwilliamson109@gmail.com>
2022-12-09 09:04:15 +0000

    main: Cleaned up day 9

--- AdventOfCode/2022/day9/main.swift
+++ AdventOfCode/2022/day9/main.swift
@@ -3,8 +3,7 @@ import Foundation
 func main() throws {
     let input: [String] = try readInput(fromTestFile: false)
     let moves = input.map { i in i.split(separator: " ").map { String($0) } }
-//    var headPosition = Coordinate(0, 0)
-//    var tailPosition = Coordinate(0, 0)
+
     var ropePositions: [Coordinate] = Array.init(repeating: Coordinate(0,0), count: 10)
     let adjustment = [
         "U": Coordinate(0, -1),
@@ -12,14 +11,13 @@ func main() throws {
         "L": Coordinate(-1, 0),
         "R": Coordinate(1, 0),
     ]
-    var visited: Set<Coordinate> = [Coordinate(0,0)]
+    var visited: [Int: Set<Coordinate>] = (1...9).reduce(into: [:], { $0[$1] = [] })
+
     for m in moves {
-//        print(m)
         let adjustment = adjustment[m[0]]!
         for _ in 0..<Int(m[1])! {
-//            var previousHeadPos = ropePositions[0]
             ropePositions[0] = ropePositions[0] + adjustment
-//            print("Head has moved to ", ropePositions[0])
+
             for j in 1..<ropePositions.count {
                 let adjacents = ropePositions[j-1].getAdjacentsIncludingSelf()
                 if !adjacents.contains(ropePositions[j]) {
@@ -28,40 +26,15 @@ func main() throws {
                     let diff = Coordinate(prev.x - curr.x, prev.y - curr.y)
                     let normalised = Coordinate(diff.x == 0 ? 0 :diff.x / abs(diff.x), diff.y == 0 ? 0 : diff.y / abs(diff.y))
                     ropePositions[j] = ropePositions[j] + normalised
-//                    visited.insert(ropePositions[9])
-//                    let newPrev = ropePositions[j]
-//                    ropePositions[j] = previousHeadPos
-////                    print("\(j) has moved to ", previousHeadPos)
-                    if j == 9 {
-//                        print(ropePositions[j])
-                        visited.insert(ropePositions[j])
-                    }
-//                    previousHeadPos = newPrev
+
+                    visited[j]?.insert(ropePositions[j])
                 }
             }
-//            printRope(ropePositions)
         }
-//        print("COMPLETE!")
-//        print(ropePositions)
     }
 
-    print(visited.count)
-}
-
-func printRope(_ rope: [Coordinate]) {
-    for y in -20..<20 {
-        var line = ""
-        for x in -20..<20 {
-            let coord = Coordinate(x, y)
-            if let index = rope.firstIndex(of: coord) {
-                line.append("\(index)")
-            } else {
-                line.append(".")
-            }
-        }
-        print(line)
-    }
-    print("\n")
+    print(visited[1]!.count)
+    print(visited[9]!.count)
 }
 
 try main()
