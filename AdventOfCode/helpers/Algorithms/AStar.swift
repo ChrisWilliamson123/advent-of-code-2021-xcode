45376dfcd70fdc290d6a87bba71ac546b5eb87da
ChrisWilliamson123 <cjwilliamson109@gmail.com>
2022-12-18 11:55:28 +0000

    main: Done day 18

--- AdventOfCode/helpers/Algorithms/AStar.swift
+++ AdventOfCode/helpers/Algorithms/AStar.swift
@@ -41,6 +41,54 @@ func aStar<T: Hashable>(graph: Set<T>,
         }
     }
 
-    assert(false, "Destination node not found")
+//    assert(false, "Destination node not found")
+    return (dist, prev)
+}
+
+func aStar<T: Hashable>(graph: Set<T>,
+                        source: T,
+                        target: T?,
+                        getNeighbours: (T) -> Set<T>,
+                        getDistanceBetween: (T, T) -> Int,
+                        heuristicFunction: (T) -> Int) -> (distances: [T: Int], chain: [T: T?]) {
+    var minutesElapsed = -1
+    var prev: [T: T?] = [:]
+    var dist: [T: Int] = [:]
+
+    for vertex in graph {
+        if vertex != source {
+            dist[vertex] = Int.max
+            prev[vertex] = nil
+        }
+    }
+
+    dist[source] = 0
+
+    var fScore: [T: Int] = [:]
+    fScore[source] = heuristicFunction(source)
+    var queue = Heap(elements: [], priorityFunction: { fScore[$0]! < fScore[$1]! })
+    queue.enqueue(source)
+    
+    while !queue.isEmpty {
+        let current = queue.dequeue()!
+        if current == target { return (dist, prev) }
+
+        for n in getNeighbours(current) {
+            let tentativeGScore = dist[current]! + getDistanceBetween(current, n)
+            if tentativeGScore < dist[n, default: Int.max] {
+                prev[n] = current
+                dist[n] = tentativeGScore
+                fScore[n] = tentativeGScore + heuristicFunction(n)
+
+                if queue.indexMap[n] != nil {
+                    queue.changeElement(n, to: n)
+                } else {
+                    queue.enqueue(n)
+                }
+            }
+        }
+    }
+
+//    assert(false, "Destination node not found")
     return (dist, prev)
 }
