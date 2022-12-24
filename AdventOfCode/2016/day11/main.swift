28557c43d1d6559a38ea536442cf37f1f27b2139
ChrisWilliamson123 <cjwilliamson109@gmail.com>
2022-12-24 16:48:25 +0000

    main: Done day 24

--- AdventOfCode/2016/day11/main.swift
+++ AdventOfCode/2016/day11/main.swift
@@ -1,193 +1,181 @@
 import Foundation
+import Algorithms
 
-func main() throws {
-    //    let input: [String] = try readInput(fromTestFile: true)
+enum Entity: Hashable, CustomStringConvertible {
+    case generator(element: String)
+    case chip(element: String)
 
-    // Test floors
-    // H = 1, G = 2
-//    let floors: [Building.Floor] = [
-//        .init(id: 0, components: [-1, -2]),
-//        .init(id: 1, components: [1]),
-//        .init(id: 2, components: [2]),
-//        .init(id: 3, components: []),
-//    ]
-//    let target = Building(elevatorFloor: 3, floors: [
-//        .init(id: 0, components: []),
-//        .init(id: 1, components: []),
-//        .init(id: 2, components: []),
-//        .init(id: 3, components: [-1, -2, 1, 2]),
-//    ])
-
-    let floors: [Building.Floor] = [
-        .init(id: 0, components: [2, -2, 4, -4]),
-        .init(id: 1, components: [1, -1, 3, -3, 5]),
-        .init(id: 2, components: [-5]),
-        .init(id: 3, components: []),
-    ]
-    let target = Building(elevatorFloor: 3, floors: [
-        .init(id: 0, components: []),
-        .init(id: 1, components: []),
-        .init(id: 2, components: []),
-        .init(id: 3, components: [1, -1, 2, -2, 3, -3, 4, -4, 5, -5]),
-    ])
-
-    let building = Building(floors: floors)
-
-    let result = dijkstra(graph: [building],
-                       source: building,
-                       target: target,
-                       getNeighbours: {
-        let ns = $0.possibleNextStates
-        return ns
-
-    }) { _, _ in
-        1
+    var description: String {
+        switch self {
+        case .generator(let element):
+            return element + "G"
+        case .chip(let element):
+            return element + "M"
+        }
     }
 
-    print(result.distances[target])
+    var element: String {
+        switch self {
+        case .generator(let element):
+            return element
+        case .chip(let element):
+            return element
+        }
+    }
 
-//    print(result.chain[target]??.printBuilding())
-    target.printBuilding()
-    print("\n")
-    var prev = result.chain[target]
-//    prev!!.printBuilding()
-    while prev != nil {
-        print("\n")
-        prev!!.printBuilding()
-        prev = result.chain[prev!!]
+
+}
+
+struct Floor: Hashable, CustomStringConvertible {
+    let entities: Set<Entity>
+
+    var description: String {
+        entities.map({ $0.description }).joined(separator: ", ")
+    }
+
+    var generators: Set<Entity> {
+        entities.reduce(into: [], { if case .generator(_) = $1 { $0.insert($1) } })
+    }
+
+    var chips: Set<Entity> {
+        entities.reduce(into: [], { if case .chip(_) = $1 { $0.insert($1) } })
     }
 }
 
-struct Building: Hashable {
+struct State: Hashable {
+    let currentFloor: Int
+    let floors: [Floor]
 
-    var elevatorFloor: Int = 0
-    var floors: [Floor]
+    var floor: Floor { floors[currentFloor] }
+    var isComplete: Bool {
+        floors[0].entities.isEmpty &&
+        floors[1].entities.isEmpty &&
+        floors[2].entities.isEmpty
+    }
+}
 
-    var possibleNextStates: Set<Building> {
-        let unrestrictedMoves = getUnrestrictedMoves()
+private let FLOOR_RANGE = 0...3
 
-        var finalMoves: Set<Move> = []
-        for unrestrictedMove in unrestrictedMoves {
-            let nextFloorComponents = floors[unrestrictedMove.floor].components
-            let generatorsOnNextFloor = nextFloorComponents.filter({ $0 > 0 })
-            let chipsOnNextFloor = nextFloorComponents.filter({ $0 < 0 })
+func main() throws {
+    let input: [String] = try readInput(fromTestFile: true)
+    let floors = buildFloors(input: input)
 
-            switch unrestrictedMove.components.count {
-            case 1:
-                // It's either a gen or a mic
-                let component = unrestrictedMove.components[0]
-                let isGenerator = component > 0
-                // If it's a generator, we can only move it if the next floor is empty OR it has one item AND the one item is the generator's microchip
-                if isGenerator && (nextFloorComponents.isEmpty || (nextFloorComponents.count == 1 && nextFloorComponents.first! == component * -1 )) {
-                    finalMoves.insert(unrestrictedMove)
-                }
+//    let currentState = State(currentFloor: 0, floors: floors)
+//    let nextStates = getNextStates(from: currentState)
+//    for state in nextStates {
+//        print("Floor: \(state.currentFloor)")
+//        print("Floors:")
+//        for f in state.floors {
+//            print("\t\(f.description)")
+//        }
+//    }
 
-                // If it's a microchip, we can move it if there's no generators on the next floor or there's the correct gen for the chip
-                if !isGenerator && (generatorsOnNextFloor.count == 0 || generatorsOnNextFloor.contains(component * -1)) {
-                    finalMoves.insert(unrestrictedMove)
-                }
-            case 2:
-                // It's either two gens, two chips or one of each
-                let first = unrestrictedMove.components[0]
-                let second = unrestrictedMove.components[1]
-                let firstIsGen = first > 0
-                let secondIsGen = second > 0
+    let currentState = State(currentFloor: 0, floors: floors)
 
-                switch (firstIsGen, secondIsGen) {
-                case (false, false):
-                    // Both are microchips, can only move them if there's no generators on the next floor OR both generators exist on the next floor
-                    if generatorsOnNextFloor.count == 0 || (generatorsOnNextFloor.contains(first * -1) && generatorsOnNextFloor.contains(second * -1)) {
-                        finalMoves.insert(unrestrictedMove)
-                    }
-                case (false, true), (true, false):
-                    // If they're a match, move them if it won't fry existing microchips on next floor
-                    if first == second * -1 {
-                        if chipsOnNextFloor.count == 0 { finalMoves.insert(unrestrictedMove) }
-                        else {
-                            let chipIsSafe: (Int) -> Bool = { chip in generatorsOnNextFloor.contains(chip * -1) }
-                            let safeChipsOnNextFloor = chipsOnNextFloor.filter({ chipIsSafe($0) })
-                            if safeChipsOnNextFloor.count == chipsOnNextFloor.count {
-                                finalMoves.insert(unrestrictedMove)
-                            }
-                        }
-                    }
-                case (true, true):
-                    // Both are gens, can move them up if there's no microchips on next floor OR if the only microchips are thr ones supported by the gens
-                    if chipsOnNextFloor.count == 0 || (chipsOnNextFloor.filter({ $0 != first * -1 && $0 != second * -1 }).count == 0) {
-                        finalMoves.insert(unrestrictedMove)
-                    }
-                }
-            default: assert(false)
-            }
+    var cache: [State: Int] = [:]
 
-
-
-
+    func bfs(state: State, seen: Set<State>, depth: Int = 0) -> Int {
+//        print(depth)
+        if let cached = cache[state] {
+            return cached
         }
-        if let optimal = finalMoves.first(where: { $0.components.count == 2 && $0.components[0] < 0 && $0.components[1] < 0 }) {
-            return [buildNewBuilding(from: optimal)]
+//        if seen.contains(state) {
+//            return Int.max
+//        }
+        if state.isComplete {
+//            print("Complete", state, depth)
+            return depth
         }
-        return finalMoves.reduce(into: [], { $0.insert(buildNewBuilding(from: $1)) })
+        var best = Int.max
+        let nextStates = getNextStates(from: state, seen: seen)
+        if nextStates.isEmpty { return best }
+        for nextState in nextStates {
+            let result = bfs(state: nextState, seen: Set(seen.union([state])), depth: depth + 1)
+            best = min(result, best)
+        }
+        cache[state] = best
+//        print(state, best)
+        return best
     }
 
-    private func buildNewBuilding(from move: Move) -> Building {
-        _ = floors
-        var newFloors = floors
-        let newElevatorFloor = move.floor
+    print(bfs(state: currentState, seen: []))
+}
 
-        // Remove the components of the move from the current floor and add them to the new floor
-        for componentToMove in move.components {
-            newFloors[elevatorFloor].components.remove(componentToMove)
-            newFloors[newElevatorFloor].components.insert(componentToMove)
-        }
-
-        return .init(elevatorFloor: newElevatorFloor, floors: newFloors)
+private func isStateValid(_ state: State) -> Bool {
+    if !FLOOR_RANGE.contains(state.currentFloor) {
+        return false
     }
 
-    private func getUnrestrictedMoves() -> [Move] {
-        let possibleNextFloorsForElevator = [max(0, elevatorFloor-1), min(floors.count - 1, elevatorFloor + 1)].filter({ $0 != elevatorFloor })
-
-        // Get a full list of possible moves, ignoring rules
-        let componentsOnCurrentFloor = Array(floors[elevatorFloor].components)
-        var componentMoves: [[Int]] = []
-        componentMoves.append([componentsOnCurrentFloor[0]])
-        for i in 1..<componentsOnCurrentFloor.count {
-            componentMoves.append([componentsOnCurrentFloor[i-1], componentsOnCurrentFloor[i]])
-            componentMoves.append([componentsOnCurrentFloor[i]])
+    for floor in state.floors {
+        let generators = floor.generators
+        if generators.isEmpty { continue }
+        let chips = floor.chips
+        let generatorElements = generators.map { $0.element }
+        let safeChips = chips.filter({ generatorElements.contains($0.element) })
+        if safeChips.count != chips.count {
+            return false
         }
+    }
+    return true
+}
 
-        var unrestrictedMoves: [Move] = []
-        for cm in componentMoves {
-            for f in possibleNextFloorsForElevator {
-                unrestrictedMoves.append(Move(floor: f, components: cm))
-            }
+private func getNextStates(from currentState: State, seen: Set<State>) -> Set<State> {
+    var states: Set<State> = []
+    let upFloor = currentState.currentFloor + 1
+    let downFloor = currentState.currentFloor - 1
+
+    states.insert(State(currentFloor: upFloor, floors: currentState.floors))
+    states.insert(State(currentFloor: downFloor, floors: currentState.floors))
+
+    let itemPermutations = currentState.floor.entities.permutations(ofCount: 2)
+    for p in itemPermutations {
+        let newCurretFloorEntites = currentState.floor.entities.subtracting(Set(p))
+        if upFloor < 4 {
+            let newUpFloorEntites = currentState.floors[upFloor].entities.union(Set(p))
+            var allEntites = currentState.floors.map { $0.entities }
+            allEntites[currentState.currentFloor] = newCurretFloorEntites
+            allEntites[upFloor] = newUpFloorEntites
+            let newFloors = allEntites.map { Floor(entities: $0) }
+            states.insert(State(currentFloor: upFloor, floors: newFloors))
         }
-
-        return unrestrictedMoves
+        if downFloor > 0 {
+            let newDownFloorEntities = currentState.floors[downFloor].entities.union(Set(p))
+            var allEntites = currentState.floors.map { $0.entities }
+            allEntites[currentState.currentFloor] = newCurretFloorEntites
+            allEntites[downFloor] = newDownFloorEntities
+            let newFloors = allEntites.map { Floor(entities: $0) }
+            states.insert(State(currentFloor: downFloor, floors: newFloors))
+        }
     }
 
-    struct Floor: Hashable {
-        let id: Int
-        var components: Set<Int>
-    }
-
-    struct Move: Hashable, CustomStringConvertible { let floor: Int, components: [Int]; var description: String { "Floor: \(floor), Comps: \(components)" }  }
-
-    func printBuilding() {
-        let map: [Int: String] = [
-            -1: "HM",
-             -2: "LM",
-             1: "HG",
-             2: "LG",
-        ]
-        var lines: [String] = []
-        for i in 0..<floors.count {
-//            lines.append("F\(i+1)\t\(elevatorFloor == i ? "E" : " ")\t\(floors[i].components.sorted().joined(separator: "\t"))")
-            lines.append("F\(i+1)\t\(elevatorFloor == i ? "E" : " ")\t\(floors[i].components.sorted().reduce("", { $0 + "\(map[$1]!)\t" }))")
+    for item in currentState.floor.entities {
+        let newCurretFloorEntites = currentState.floor.entities.subtracting([item])
+        if upFloor < 4 {
+            let newUpFloorEntites = currentState.floors[upFloor].entities.union([item])
+            var allEntites = currentState.floors.map { $0.entities }
+            allEntites[currentState.currentFloor] = newCurretFloorEntites
+            allEntites[upFloor] = newUpFloorEntites
+            let newFloors = allEntites.map { Floor(entities: $0) }
+            states.insert(State(currentFloor: upFloor, floors: newFloors))
         }
-        lines.reverse()
-        for l in lines { print(l) }
+        if downFloor > 0 {
+            let newDownFloorEntities = currentState.floors[downFloor].entities.union([item])
+            var allEntites = currentState.floors.map { $0.entities }
+            allEntites[currentState.currentFloor] = newCurretFloorEntites
+            allEntites[downFloor] = newDownFloorEntities
+            let newFloors = allEntites.map { Floor(entities: $0) }
+            states.insert(State(currentFloor: downFloor, floors: newFloors))
+        }
     }
+    return states.filter({ isStateValid($0) }).subtracting(seen)
+}
+
+private func buildFloors(input: [String]) -> [Floor] {
+    input.map({
+        let chips = Regex("(\\w+)-compatible").getGreedyMatches(in: $0).map { chip in Entity.chip(element: chip.components(separatedBy: "-")[0][0].uppercased()) }
+        let generators = Regex("\\w+ generator").getGreedyMatches(in: $0).map { generator in Entity.generator(element: generator.components(separatedBy: " ")[0][0].uppercased()) }
+        return Floor(entities: Set(chips + generators))
+    })
 }
 
 Timer.time(main)
