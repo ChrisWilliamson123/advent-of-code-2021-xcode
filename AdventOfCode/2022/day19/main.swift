5d74b20aee7a158ca0f68ef77e7eab41afd64d9f
ChrisWilliamson123 <cjwilliamson109@gmail.com>
2022-12-27 15:03:15 +0000

    2022-day-19: Got day 19 down to 4.5s runtime.

--- AdventOfCode/2022/day19/main.swift
+++ AdventOfCode/2022/day19/main.swift
@@ -16,9 +16,61 @@ struct Blueprint {
     let geoOreCost: Int
     let geoObsCost: Int
 
-    var maxOreRequirement: Int { [oreCost, clayCost, obOreCost, geoOreCost].max()! }
-    var maxClayRequirement: Int { obClayCost }
-    var maxObsRequirement: Int { geoObsCost }
+    let maxOreRequirement: Int
+    let maxClayRequirement: Int
+    let maxObsRequirement: Int
+
+    init(id: Int, oreCost: Int, clayCost: Int, obOreCost: Int, obClayCost: Int, geoOreCost: Int, geoObsCost: Int) {
+        self.id = id
+        self.oreCost = oreCost
+        self.clayCost = clayCost
+        self.obOreCost = obOreCost
+        self.obClayCost = obClayCost
+        self.geoOreCost = geoOreCost
+        self.geoObsCost = geoObsCost
+
+        self.maxOreRequirement = [oreCost, clayCost, obOreCost, geoOreCost].max()!
+        self.maxClayRequirement = obClayCost
+        self.maxObsRequirement = geoObsCost
+
+    }
+
+    func getMaxGeodesPossible(in time: Int = 24) -> Int {
+        var cache: [State: Int] = [:]
+        func dfs(state: State) -> Int {
+            if let cached = cache[state] { return cached }
+            if state[0] == time { return state[4] }
+
+            var bestGeodesRetrieved = 0
+            let nextStates = getNextStates(from: state, using: self, endTime: time)
+            for state in nextStates {
+                let result = dfs(state: state)
+                bestGeodesRetrieved = max(result, bestGeodesRetrieved)
+            }
+            cache[state] = bestGeodesRetrieved
+            return bestGeodesRetrieved
+        }
+
+        let initialState = [0, 0, 0, 0, 0, 1, 0, 0, 0]
+        return dfs(state: initialState)
+    }
+}
+
+private extension State {
+    func pruned(for blueprint: Blueprint, endTime: Int) -> State {
+        let (timeElapsed, ore, clay, obs, geo, oreR, clayR, obsR, geoR) = destructureState(self)
+        let timeRemaining = endTime - timeElapsed - 1
+
+        return [timeElapsed,
+                ore > timeRemaining * blueprint.maxOreRequirement ? timeRemaining * blueprint.maxOreRequirement : ore,
+                clay > timeRemaining * blueprint.maxClayRequirement ? timeRemaining * blueprint.maxClayRequirement : clay,
+                obs > timeRemaining * blueprint.maxObsRequirement ? timeRemaining * blueprint.maxObsRequirement : obs,
+                geo,
+                oreR,
+                clayR,
+                obsR,
+                geoR]
+    }
 }
 
 // STATE
@@ -34,39 +86,26 @@ func main() throws {
     }
 
     let queue = OperationQueue()
-
     var results: [Int] = []
     for b in blueprints {
-        var cache: [State: Int] = [:]
-        func dfs(state: State, endTime: Int = 24) -> Int {
-            if let cached = cache[state] { return cached }
-            let (timeElapsed, ore, clay, obs, geo, oreR, clayR, obsR, geoR) = destructureState(state)
-            if timeElapsed == endTime { return geo }
-
-            var bestGeodesRetrieved = 0
-            let nextStates = getNextStates(from: state, using: b)
-            for state in nextStates {
-                let result = dfs(state: state, endTime: endTime)
-                bestGeodesRetrieved = max(result, bestGeodesRetrieved)
-            }
-            cache[state] = bestGeodesRetrieved
-            return bestGeodesRetrieved
-        }
-
-        let initialState = [0, 0, 0, 0, 0, 1, 0, 0, 0]
         queue.addOperation({
-            let result = dfs(state: initialState)
-            print("Blueprint \(b.id):", result)
-            results.append(result * b.id)
+            results.append(b.getMaxGeodesPossible() * b.id)
         })
     }
-
     queue.waitUntilAllOperationsAreFinished()
-
     print(results.sum())
+
+    results.removeAll()
+    for b in blueprints[0..<3] {
+        queue.addOperation({
+            results.append(b.getMaxGeodesPossible(in: 32))
+        })
+    }
+    queue.waitUntilAllOperationsAreFinished()
+    print(results.multiply())
 }
 
-private func getNextStates(from state: [Int], using blueprint: Blueprint) -> Set<State> {
+private func getNextStates(from state: [Int], using blueprint: Blueprint, endTime: Int) -> Set<State> {
     /**
      THERES NO USE IN HAVING MORE OF A MATERIAL THAN WHAT THE GREEDIEST ROBOT CAN USE
         Therefore if we have excess materials, we can just reduce them down to the maximum needed?
@@ -74,27 +113,30 @@ private func getNextStates(from state: [Int], using blueprint: Blueprint) -> Set
      */
     let (timeElapsed, ore, clay, obs, geo, oreR, clayR, obsR, geoR) = destructureState(state)
     var states: Set<State> = []
-    // just gather ore
-    states.insert([timeElapsed + 1, ore + oreR, clay + clayR, obs + obsR, geo + geoR, oreR, clayR, obsR, geoR])
-
-    // Try to build each robot
-    // ore
-    if oreR < blueprint.maxOreRequirement && ore >= blueprint.oreCost {
-        states.insert([timeElapsed + 1, ore + oreR - blueprint.oreCost, clay + clayR, obs + obsR, geo + geoR, oreR + 1, clayR, obsR, geoR])
-    }
-    // clay
-    if clayR < blueprint.maxClayRequirement && ore >= blueprint.clayCost {
-        states.insert([timeElapsed + 1, ore + oreR - blueprint.clayCost, clay + clayR, obs + obsR, geo + geoR, oreR, clayR + 1, obsR, geoR])
-    }
-    // obsidian
-    if obsR < blueprint.maxObsRequirement && ore >= blueprint.obOreCost && clay >= blueprint.obClayCost {
-        states.insert([timeElapsed + 1, ore + oreR - blueprint.obOreCost, clay + clayR - blueprint.obClayCost, obs + obsR, geo + geoR, oreR, clayR, obsR + 1, geoR])
-    }
-    // geode
+    // Try to build a geode robot, use only this state if we can build one
     if ore >= blueprint.geoOreCost && obs >= blueprint.geoObsCost {
-        // If we can build a geode robot, always do that
-        let state = [timeElapsed + 1, ore + oreR - blueprint.geoOreCost, clay + clayR, obs + obsR - blueprint.geoObsCost, geo + geoR, oreR, clayR, obsR, geoR + 1]
-//        return [state]
+        let state = [timeElapsed + 1, ore + oreR - blueprint.geoOreCost, clay + clayR, obs + obsR - blueprint.geoObsCost, geo + geoR, oreR, clayR, obsR, geoR + 1].pruned(for: blueprint, endTime: endTime)
+        return [state]
+    }
+    // Try to build an obsidian robot, use only this state if we can build one
+    if obsR < blueprint.maxObsRequirement && ore >= blueprint.obOreCost && clay >= blueprint.obClayCost {
+        let state = [timeElapsed + 1, ore + oreR - blueprint.obOreCost, clay + clayR - blueprint.obClayCost, obs + obsR, geo + geoR, oreR, clayR, obsR + 1, geoR].pruned(for: blueprint, endTime: endTime)
+        return [state]
+    }
+
+    // Just gather ore, no building
+    let state = [timeElapsed + 1, ore + oreR, clay + clayR, obs + obsR, geo + geoR, oreR, clayR, obsR, geoR].pruned(for: blueprint, endTime: endTime)
+    states.insert(state)
+
+    // Try to build a clay robot
+    if clayR < blueprint.maxClayRequirement && ore >= blueprint.clayCost {
+        let state = [timeElapsed + 1, ore + oreR - blueprint.clayCost, clay + clayR, obs + obsR, geo + geoR, oreR, clayR + 1, obsR, geoR].pruned(for: blueprint, endTime: endTime)
+        states.insert(state)
+    }
+
+    // Try to build an ore robot
+    if oreR < blueprint.maxOreRequirement && ore >= blueprint.oreCost {
+        let state = [timeElapsed + 1, ore + oreR - blueprint.oreCost, clay + clayR, obs + obsR, geo + geoR, oreR + 1, clayR, obsR, geoR].pruned(for: blueprint, endTime: endTime)
         states.insert(state)
     }
 
