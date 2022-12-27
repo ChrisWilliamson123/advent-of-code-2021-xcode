58698a07aa5006d4965a34eae6e94eb63b6f5810
ChrisWilliamson123 <cjwilliamson109@gmail.com>
2022-12-27 15:14:29 +0000

    2022-day-19: Made 19 more performant by using a struct rather than array of ints for state.

--- AdventOfCode/2022/day19/main.swift
+++ AdventOfCode/2022/day19/main.swift
@@ -39,7 +39,7 @@ struct Blueprint {
         var cache: [State: Int] = [:]
         func dfs(state: State) -> Int {
             if let cached = cache[state] { return cached }
-            if state[0] == time { return state[4] }
+            if state.t == time { return state.g }
 
             var bestGeodesRetrieved = 0
             let nextStates = getNextStates(from: state, using: self, endTime: time)
@@ -51,31 +51,51 @@ struct Blueprint {
             return bestGeodesRetrieved
         }
 
-        let initialState = [0, 0, 0, 0, 0, 1, 0, 0, 0]
+        let initialState = State(0, 0, 0, 0, 0, 1, 0, 0, 0)
         return dfs(state: initialState)
     }
 }
 
-private extension State {
-    func pruned(for blueprint: Blueprint, endTime: Int) -> State {
-        let (timeElapsed, ore, clay, obs, geo, oreR, clayR, obsR, geoR) = destructureState(self)
-        let timeRemaining = endTime - timeElapsed - 1
+struct State: Hashable {
+    let t: Int
+    let o: Int
+    let c: Int
+    let ob: Int
+    let g: Int
+    let oR: Int
+    let cR: Int
+    let obR: Int
+    let gR: Int
 
-        return [timeElapsed,
-                ore > timeRemaining * blueprint.maxOreRequirement ? timeRemaining * blueprint.maxOreRequirement : ore,
-                clay > timeRemaining * blueprint.maxClayRequirement ? timeRemaining * blueprint.maxClayRequirement : clay,
-                obs > timeRemaining * blueprint.maxObsRequirement ? timeRemaining * blueprint.maxObsRequirement : obs,
-                geo,
-                oreR,
-                clayR,
-                obsR,
-                geoR]
+    init(_ t: Int, _ o: Int, _ c: Int, _ ob: Int, _ g: Int, _ oR: Int, _ cR: Int, _ obR: Int, _ gR: Int) {
+        self.t = t
+        self.o = o
+        self.c = c
+        self.ob = ob
+        self.g = g
+        self.oR = oR
+        self.cR = cR
+        self.obR = obR
+        self.gR = gR
+    }
+
+    func pruned(for blueprint: Blueprint, endTime: Int) -> State {
+        let timeRemaining = endTime - t - 1
+        let maxPossibleOreUse = timeRemaining * blueprint.maxOreRequirement
+        let maxPossibleClayUse = timeRemaining * blueprint.maxClayRequirement
+        let maxPossibleObsUse = timeRemaining * blueprint.maxObsRequirement
+        return .init(t,
+                     o > maxPossibleOreUse ? maxPossibleOreUse : o,
+                     c > maxPossibleClayUse ? timeRemaining * maxPossibleClayUse : c,
+                     ob > maxPossibleObsUse ? timeRemaining * maxPossibleObsUse : ob,
+                     g,
+                     oR,
+                     cR,
+                     obR,
+                     gR)
     }
 }
 
-// STATE
-// [ timeElapsed, ore, clay, obs, geo, oreR, clayR, obsR, geoR ]
-typealias State = [Int]
 func main() throws {
     let input: [String] = try readInput(fromTestFile: false)
     let blueprints = input.map {
@@ -105,38 +125,47 @@ func main() throws {
     print(results.multiply())
 }
 
-private func getNextStates(from state: [Int], using blueprint: Blueprint, endTime: Int) -> Set<State> {
+private func getNextStates(from state: State, using blueprint: Blueprint, endTime: Int) -> Set<State> {
     /**
      THERES NO USE IN HAVING MORE OF A MATERIAL THAN WHAT THE GREEDIEST ROBOT CAN USE
-        Therefore if we have excess materials, we can just reduce them down to the maximum needed?
+     Therefore if we have excess materials, we can just reduce them down to the maximum needed?
      THERES ALSO NO USE IN HAVING MORE ROBOTS THAN THE NUMBER OF MATERIALS WE CAN USE
      */
-    let (timeElapsed, ore, clay, obs, geo, oreR, clayR, obsR, geoR) = destructureState(state)
+//    let (timeElapsed, ore, clay, obs, geo, oreR, clayR, obsR, geoR) = destructureState(state)
+    let timeElapsed = state.t
+    let ore = state.o
+    let clay = state.c
+    let obs = state.ob
+    let geo = state.g
+    let oreR = state.oR
+    let clayR = state.cR
+    let obsR = state.obR
+    let geoR = state.gR
     var states: Set<State> = []
     // Try to build a geode robot, use only this state if we can build one
     if ore >= blueprint.geoOreCost && obs >= blueprint.geoObsCost {
-        let state = [timeElapsed + 1, ore + oreR - blueprint.geoOreCost, clay + clayR, obs + obsR - blueprint.geoObsCost, geo + geoR, oreR, clayR, obsR, geoR + 1].pruned(for: blueprint, endTime: endTime)
+        let state = State(timeElapsed + 1, ore + oreR - blueprint.geoOreCost, clay + clayR, obs + obsR - blueprint.geoObsCost, geo + geoR, oreR, clayR, obsR, geoR + 1).pruned(for: blueprint, endTime: endTime)
         return [state]
     }
     // Try to build an obsidian robot, use only this state if we can build one
     if obsR < blueprint.maxObsRequirement && ore >= blueprint.obOreCost && clay >= blueprint.obClayCost {
-        let state = [timeElapsed + 1, ore + oreR - blueprint.obOreCost, clay + clayR - blueprint.obClayCost, obs + obsR, geo + geoR, oreR, clayR, obsR + 1, geoR].pruned(for: blueprint, endTime: endTime)
+        let state = State(timeElapsed + 1, ore + oreR - blueprint.obOreCost, clay + clayR - blueprint.obClayCost, obs + obsR, geo + geoR, oreR, clayR, obsR + 1, geoR).pruned(for: blueprint, endTime: endTime)
         return [state]
     }
 
     // Just gather ore, no building
-    let state = [timeElapsed + 1, ore + oreR, clay + clayR, obs + obsR, geo + geoR, oreR, clayR, obsR, geoR].pruned(for: blueprint, endTime: endTime)
+    let state = State(timeElapsed + 1, ore + oreR, clay + clayR, obs + obsR, geo + geoR, oreR, clayR, obsR, geoR).pruned(for: blueprint, endTime: endTime)
     states.insert(state)
 
     // Try to build a clay robot
     if clayR < blueprint.maxClayRequirement && ore >= blueprint.clayCost {
-        let state = [timeElapsed + 1, ore + oreR - blueprint.clayCost, clay + clayR, obs + obsR, geo + geoR, oreR, clayR + 1, obsR, geoR].pruned(for: blueprint, endTime: endTime)
+        let state = State(timeElapsed + 1, ore + oreR - blueprint.clayCost, clay + clayR, obs + obsR, geo + geoR, oreR, clayR + 1, obsR, geoR).pruned(for: blueprint, endTime: endTime)
         states.insert(state)
     }
 
     // Try to build an ore robot
     if oreR < blueprint.maxOreRequirement && ore >= blueprint.oreCost {
-        let state = [timeElapsed + 1, ore + oreR - blueprint.oreCost, clay + clayR, obs + obsR, geo + geoR, oreR + 1, clayR, obsR, geoR].pruned(for: blueprint, endTime: endTime)
+        let state = State(timeElapsed + 1, ore + oreR - blueprint.oreCost, clay + clayR, obs + obsR, geo + geoR, oreR + 1, clayR, obsR, geoR).pruned(for: blueprint, endTime: endTime)
         states.insert(state)
     }
 
