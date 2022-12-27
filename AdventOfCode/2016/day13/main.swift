7f897f82ced95ab0c4e5936bd4ddd569c974b1b8
ChrisWilliamson123 <cjwilliamson109@gmail.com>
2022-12-27 17:20:25 +0000

    main: Done 2016 day 13 and 14

--- AdventOfCode/2016/day13/main.swift
+++ AdventOfCode/2016/day13/main.swift
@@ -1,7 +1,39 @@
 import Foundation
 
 func main() throws {
-    let input: [String] = try readInput(fromTestFile: false)
+    let input = 1350
+
+    let start = Coordinate(1, 1)
+
+    func bfs(start: Coordinate, destination: Coordinate, stopAt: Int? = nil) -> Int {
+        var frontier: [(pos: Coordinate, distance: Int)] = [(start, 0)]
+        var explored: Set<Coordinate> = [start]
+        while !frontier.isEmpty {
+            let (currentPos, distance) = frontier.popLast()!
+            if currentPos == destination { return distance }
+
+            let axialAdjacents = currentPos.getAxialAdjacents().filter({ $0.x >= 0 && $0.y >= 0 })
+
+            for a in axialAdjacents {
+                if !explored.contains(a) {
+                    if isOpenSpace(a, input: input) {
+                        if distance == stopAt { return explored.count }
+                        frontier.insert((a, distance + 1), at: 0)
+                        explored.insert(a)
+                    }
+                }
+            }
+        }
+        assert(false)
+    }
+
+    print(bfs(start: start, destination: Coordinate(31,39)))
+    print(bfs(start: start, destination: Coordinate(1000,1000), stopAt: 50))
+}
+
+private func isOpenSpace(_ coord: Coordinate, input: Int) -> Bool {
+    let binaryRepresentation = String(((coord.x * coord.x) + (3*coord.x) + (2 * coord.x * coord.y) + coord.y + (coord.y * coord.y) + input), radix: 2)
+    return binaryRepresentation.filter({ $0 == "1" }).count % 2 == 0
 }
 
 Timer.time(main)
--- AdventOfCode/2016/day14/main.swift
+++ AdventOfCode/2016/day14/main.swift
@@ -1,7 +1,59 @@
 import Foundation
+import CryptoKit
 
 func main() throws {
-    let input: [String] = try readInput(fromTestFile: false)
+    let input = "ihaygndm"
+    var index = 0
+    var otpKeyIndexes: [Int] = []
+
+    let queue = OperationQueue()
+    var hashDicts: [[Int: String]] = []
+    let threadSize = 5000
+    for i in stride(from: 0, to: 50000, by: threadSize) {
+        queue.addOperation({
+            var hashes: [Int: String] = [:]
+            for j in i..<i+threadSize {
+                var hash = MD5(string: "\(input)\(j)")
+                for _ in 0..<2016 { hash = MD5(string: hash) }
+                hashes[j] = hash
+            }
+            hashDicts.append(hashes)
+        })
+    }
+    queue.waitUntilAllOperationsAreFinished()
+    let hashes = hashDicts.reduce(into: [Int: String](), { $0.merge($1, uniquingKeysWith: { $1 }) })
+    while otpKeyIndexes.count < 64 {
+        let hash = hashes[index]!
+        if let triple = hash.triple() {
+            for i in index+1...index+1000 {
+                let hash = hashes[i]!
+                let pent = "\(triple)\(triple)\(triple)\(triple)\(triple)"
+                if hash.contains(pent) {
+                    otpKeyIndexes.append(index)
+                    break
+                }
+            }
+        }
+        index += 1
+    }
+
+    print(otpKeyIndexes[otpKeyIndexes.count-1])
+}
+
+private extension String {
+    func triple() -> String? {
+        for i in 2..<self.count {
+            if Set([self[i-2], self[i-1], self[i]]).count == 1 {
+                return self[i]
+            }
+        }
+        return  nil
+    }
+}
+
+
+func MD5(string: String) -> String {
+    Insecure.MD5.hash(data: string.data(using: .utf8) ?? Data()).description[12..<44]
 }
 
 Timer.time(main)
